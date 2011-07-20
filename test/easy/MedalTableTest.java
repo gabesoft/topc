2cacaecadd629e8778668e13ee482c20b205fb3b
gabriel <gabesoft@gmail.com>
2011-07-19 22:02:03 -0700

    indentation

--- test/easy/MedalTableTest.java
+++ test/easy/MedalTableTest.java
@@ -9,7 +9,8 @@ import topc.easy.*;
 public class MedalTableTest {
   MedalTable _calc = new MedalTable();
 
-  @Test public void testCase1() {
+  @Test 
+  public void testCase1() {
     String[] data = {"ITA JPN AUS", "KOR TPE UKR", "KOR KOR GBR", "KOR CHN TPE"};
     String[] expected = { 
       "KOR 3 1 0",
