301ca2a4d152c6cc6a58e00a0de999cb03f0e75a
gabriel <gabesoft@gmail.com>
2011-09-04 00:18:23 -0700

    team builder stub

---
+++ src/graph/TeamBuilder.java
@@ -0,0 +1,17 @@
+package topc.graph;
+
+import java.util.*;
+import java.io.*;
+
+// SRM 184 Division II Level Three - 1000
+// graph theory
+// http://community.topcoder.com/stat?c=problem_statement&pm=2356&rd=4740
+public class TeamBuilder {
+  public int[] specialLocations(String[] paths) {
+    return null;
+  }
+
+  private void debug(Object... os) {
+    System.out.println(Arrays.deepToString(os));
+  }
+}
