54be3cd0bd323e248b691309f5a52e2476ce3e40
gabriel <gabesoft@gmail.com>
2011-09-10 10:39:07 -0700

    removed unnecessary files

---
+++ src/dynamic/MiniPaint.java
@@ -0,0 +1,17 @@
+package topc.dynamic;
+
+import java.util.*;
+import java.io.*;
+
+// SRM 178 Division I Level Three - 1000
+// dynamic programming
+// http://community.topcoder.com/stat?c=problem_statement&pm=1996&rd=4710
+public class MiniPaint {
+  public int leastBad(String[] picture, int maxStrokes) {
+    return 0;
+  }
+
+  private void debug(Object... os) {
+    System.out.println(Arrays.deepToString(os));
+  }
+}
