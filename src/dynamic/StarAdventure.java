5474d1786a412eb1266a59586b065fe3a7f429cd
gabriel <gabesoft@gmail.com>
2011-09-11 20:30:16 -0700

    star adventure speedup

--- src/dynamic/StarAdventure.java
+++ src/dynamic/StarAdventure.java
@@ -10,7 +10,7 @@ public class StarAdventure {
   int N;
   int M;
   int[][] data;
-  int[][][][] best;
+  int[][][] best;
   int[][][] sums;
 
   public int mostStars(String[] level) {
@@ -30,28 +30,29 @@ public class StarAdventure {
 
     if (M <= 3) { return sumAll(); }
 
-    best = new int[N][M][M][M];
+    best = new int[M][M][M];
     for (int y = 0; y < N; y++) {
+      int[][][] curr = new int[M][M][M];
       for (int i = 0; i < M - 2; i++) {
         for (int j = i + 1; j < M - 1; j++) {
           for (int k = j + 1; k < M; k++) {
-            best[y][i][j][k] = findBest(y, i, j, k);
+            curr[i][j][k] = findBest(y, i, j, k);
           }
         }
       }
+      best = curr;
     }
 
-    return best[N - 1][M - 3][M - 2][M - 1];
+    return best[M - 3][M - 2][M - 1];
   }
 
   int findBest(int row, int i, int j, int k) {
     int curr = 0;
-    int[][][] prev = row == 0 ? new int[M][M][M] : best[row - 1];
 
     for (int ip = 0; ip < i + 1; ip++) {
       for (int jp = i + 1; jp < j + 1; jp++) {
         for (int kp = j + 1; kp < k + 1; kp++) {
-          curr = Math.max(curr, prev[ip][jp][kp] + sums[row][ip][i] + sums[row][jp][j] + sums[row][kp][k]);
+          curr = Math.max(curr, best[ip][jp][kp] + sums[row][ip][i] + sums[row][jp][j] + sums[row][kp][k]);
         }
       }
     }
