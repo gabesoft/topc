30f43807775f6c5d040b1279cb0a894dc4b39f6e
gabriel <gabesoft@gmail.com>
2011-09-17 12:32:45 -0700

    weird rooks - stubs

---
+++ src/easy/WeirdRooks.java
@@ -0,0 +1,17 @@
+package topc.easy;
+
+import java.util.*;
+import java.io.*;
+
+// SRM 234 Division I Level Two - 650
+// brute force, recursion
+// http://community.topcoder.com/stat?c=problem_statement&pm=3998&rd=6533
+public class WeirdRooks {
+  public String describe(int[] cols) {
+    return null;
+  }
+
+  private void debug(Object... os) {
+    System.out.println(Arrays.deepToString(os));
+  }
+}
