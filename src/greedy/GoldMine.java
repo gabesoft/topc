a69b85f787c9abb1244ddc35a2200407e523b427
gabriel <gabesoft@gmail.com>
2011-09-22 06:05:30 -0700

    gold mine - complete

--- src/greedy/GoldMine.java
+++ src/greedy/GoldMine.java
@@ -7,10 +7,11 @@ import java.io.*;
 // greedy, math
 // http://community.topcoder.com/stat?c=problem_statement&pm=1957&rd=4650
 public class GoldMine {
+  double DELTA = 0.0001;
   int W = 7;        // max workers per mine
   int N;            // mine count
   double[][] perc;
-  int[][] profit;
+  double[][] profit;
 
   public int[] getAllocation(String[] mines, int miners) {
     N = mines.length;
@@ -26,40 +27,35 @@ public class GoldMine {
 
     calculateProfit();
 
-    // TODO: remove debug code below
-    debug("perc", perc);
-    for (int i = 0; i < N; i++) {
-      debug(i, profit[i]);
-    }
-
     int[] alloc = new int[N];
     for (int i = 0; i < miners; i++) {
       int mine = -1;
-      int best = Integer.MIN_VALUE;
+      double best = Double.MAX_VALUE / -2;
       for (int j = 0; j < N; j++) {
-        int curr = profitIncrease(j, alloc[j]);
-        if (curr > best) {
+        double curr = profitIncrease(j, alloc[j]);
+
+        if (curr > best + DELTA) {
           mine = j;
           best = curr;
         }
       }
+
       if (mine > -1) { alloc[mine]++; }
     }
 
-    debug("alloc", alloc);
     return alloc;
   }
 
-  int profitIncrease(int mine, int workers) {
+  double profitIncrease(int mine, int workers) {
     if (workers >= W - 1) {
-      return Integer.MIN_VALUE + 1;
+      return Double.MAX_VALUE / -4;
     } else {
       return profit[mine][workers + 1] - profit[mine][workers];
     }
   }
 
   void calculateProfit() {
-    profit = new int[N][W];
+    profit = new double[N][W];
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < W; j++) {
         profit[i][j] = calculateProfit(perc[i], j);
@@ -67,7 +63,7 @@ public class GoldMine {
     }
   }
 
-  int calculateProfit(double[] perc, int workerCount) {
+  double calculateProfit(double[] perc, int workerCount) {
     double profit = 0;
     for (int i = workerCount + 1; i < W; i++) {
       profit += perc[i] * 60 * (double)workerCount;
@@ -76,7 +72,7 @@ public class GoldMine {
       profit += perc[i] * 50 * (double)i;
       profit -= perc[i] * 20 * (double)(workerCount - i);
     }
-    return (int)profit;
+    return profit;
   }
 
   private void debug(Object... os) {
