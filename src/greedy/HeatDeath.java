e86ff34aa11654df3ffdd4f4b03c8ac2c03a2b77
gabriel <gabesoft@gmail.com>
2011-09-28 18:42:14 -0700

    heat death - in progress 2

--- src/greedy/HeatDeath.java
+++ src/greedy/HeatDeath.java
@@ -12,32 +12,66 @@ public class HeatDeath {
     int time = 0;
     boolean done = false;
 
+    int avg = 0;
+    for (int i = 0; i < n; i++) {
+      avg += energy[i];
+    }
+    avg /= n;
+    debug("avg", avg);
+
     while (!done) {
       int a = -1;
       int b = -1;
-      int min = 999;
+      int v = avg;
+      int u = avg;
+      //int min = 999;
+      //
 
-      Arrays.sort(energy);
+      debug("energy", energy);
+
+      //Arrays.sort(energy);
       for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-          int dx = Math.abs(energy[i] - energy[j]);
-          if (i != j && dx > 1) {
-            if (min >= dx) {
-              min = dx;
+          int dx = (energy[i] + energy[j]) / 2;
+          int dy = Math.abs(energy[i] - energy[j]);
+          //debug(i, j, dy, dx);
+          if (i != j && dy > 1) {
+            if (a == -1 && b == -1) {
               a = i;
               b = j;
+              v = energy[a] + (energy[a] > energy[b] ? -1 : 1);
+              u = energy[b] + (energy[b] > energy[a] ? -1 : 1);
+            }
+
+            int p = dx;
+            int q = (energy[i] + energy[j]) % 2 == 0 ? dx : dx + 1;
+            int cur = Math.abs((avg - p) + (avg - q));
+            int old = Math.abs((avg - v) + (avg - u));
+            //int cur = Math.abs(avg - p) + Math.abs(avg - q);
+            //int old = Math.abs(avg - v) + Math.abs(avg - u);
+            if (cur > old) {
+              debug("dx", dx, energy[i], energy[j], cur, old, p, q);
+              a = i;
+              b = j;
+              v = p;
+              u = q;
             }
           }
         }
       }
 
+
       if (a > -1 && b > -1) {
-        time += 1;
-        energy[a] += energy[a] > energy[b] ? -1 : 1;
-        energy[b] += energy[b] > energy[a] ? -1 : 1;
+        debug(time, a, b, energy[a], "->", v, energy[b], "->", u, energy);
+        time += Math.min(u, v) - Math.min(energy[a], energy[b]);
+        energy[a] = v;
+        energy[b] = u;
+        //time += 1;
+        //energy[a] += energy[a] > energy[b] ? -1 : 1;
+        //energy[b] += energy[b] > energy[a] ? -1 : 1;
         a = -1;
         b = -1;
-        min = 999;
+        //min = 999;
       } else {
         done = true;
       }
