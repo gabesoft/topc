d20c09df3367834b3753b2a455f20458b1350a77
gabriel <gabesoft@gmail.com>
2011-10-08 23:01:21 -0700

    rockskipping - cleanup

--- src/dynamic/RockSkipping.java
+++ src/dynamic/RockSkipping.java
@@ -11,46 +11,37 @@ public class RockSkipping {
   public double probability(String pads, int maxDist) {
     int n = maxDist * (maxDist + 1) / 2 + 1;
 
-    int[] p = new int[n];
+    int[] lake = new int[n];
     for (int i = 0; i < n; i++) {
-      p[i] = pads.charAt(i % pads.length()) == 'X' ? 0 : 1;
+      lake[i] = pads.charAt(i % pads.length()) == 'X' ? 0 : 1;
     }
 
-    int lim = 1;
-    double[] w = new double[n];
-
-    w[0] = 1.0;
+    int maxd = 1;
+    double[] probs = new double[n];
+    probs[0] = 1.0;
     for (int i = 0; i < maxDist; i++) {
 
       int dist = maxDist - i;
-      double s = 1.0 / dist;
-      double[] c = new double[n];
+      double pskip = 1.0 / dist;
+      double[] curr = new double[n];
+      for (int j = i; j < maxd; j++) {
+        if (probs[j] == 0.0) { continue; }
 
-      for (int j = 0; j < lim; j++) {
-        if (w[j] == 0.0) { continue; }
         for (int k = j + 1; k < j + dist + 1; k++) {
-          c[k] += p[k] * s * w[j];
+          curr[k] += lake[k] * pskip * probs[j];
         }
       }
 
-      lim += dist;
-      w = c;
+      maxd += dist;
+      probs = curr;
     }
 
     double prob = 0.0;
     for (int i = 0; i < n; i++) {
-      prob += w[i];
+      prob += probs[i];
     }
 
     return prob * 100;
-  }
-
-  int sum(double[] arr) {
-    int res = 0;
-    for (int i = 0; i < arr.length; i++) {
-      res += arr[i] == 0.0 ? 0 : 1;
-    }
-    return res;
   }
 
   private void debug(Object... os) {
