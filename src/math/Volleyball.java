21f8c55160d058f60249ec9b490bfa15be49dbec
gabriel <gabesoft@gmail.com>
2011-10-10 19:24:42 -0700

    volleyball - complete

--- src/math/Volleyball.java
+++ src/math/Volleyball.java
@@ -8,40 +8,34 @@ import java.io.*;
 // statement: http://community.topcoder.com/stat?c=problem_statement&pm=2959&rd=5880
 // editorial: http://www.topcoder.com/tc?module=Static&d1=match_editorials&d2=tco04_online_rd_3
 public class Volleyball {
-  int LIM = 92;
+  int LIM = 5000;
+  double[][][] memo;
   double sProb;
   double rProb;
-  double sWin;
 
   public double win(int sScore, int rScore, double probWinServe) {
+    memo  = new double[2][LIM / 2 + 10][LIM / 2 + 10];
+
     sProb = probWinServe;
     rProb = 1.0 - sProb;
 
-    prob(sScore, rScore, 1.0, 1);
-
-    return sWin;
+    return prob(sScore, rScore, 1);
   }
 
-  //double[][][] memo = new double[2][LIM][LIM];
-  void prob(int s, int r, double p, int sTeam) {
-    //debug(s, r, sTeam);
-    //if (memo[sTeam][s][r] > 0) {
-      //return;
-    //}
+  double prob(int s, int r, int sTeam) {
+    if (memo[sTeam][s][r] > 0.0) { return memo[sTeam][s][r]; }
 
-    if (win(r, s)) { 
-      sWin += (1 - sTeam) * p;
-      return;
-    }
-    if (win(s, r)) {
-      sWin += sTeam * p;
-      return;
-    }
-    if (s + r > LIM) { return; }
+    if (win(r, s)) { return 1 - sTeam; }
+    if (win(s, r)) { return sTeam; }
 
-    //memo[sTeam][s][r] = 1.0;
-    prob(s + 1, r, p * sProb, sTeam);
-    prob(r + 1, s, p * rProb, 1 - sTeam);
+    if (s + r > LIM) { return 0.0; }
+
+    double res = 0.0;
+    res += sProb * prob(s + 1, r, sTeam);
+    res += rProb * prob(r + 1, s, 1 - sTeam);
+
+    memo[sTeam][s][r] = res;
+    return res;
   }
 
   boolean win(int aScore, int bScore) {
--- test/math/VolleyballTest.java
+++ test/math/VolleyballTest.java
@@ -9,7 +9,7 @@ public class VolleyballTest {
   double DELTA = 1.0e-09;
   Volleyball volleyball = new Volleyball();
 
-  //@Test
+  @Test
   public void case1() {
     int sScore = 13;
     int rScore = 13;
@@ -17,7 +17,7 @@ public class VolleyballTest {
     assertEquals(0.5, volleyball.win(sScore, rScore, probWinServe), DELTA * 0.5);
   }
 
-  //@Test
+  @Test
   public void case2() {
     int sScore = 1;
     int rScore = 14;
@@ -25,7 +25,7 @@ public class VolleyballTest {
     assertEquals(3.355704697986578e-27, volleyball.win(sScore, rScore, probWinServe), DELTA * 3.355704697986578e-27);
   }
 
-  //@Test
+  @Test
   public void case3() {
     int sScore = 8;
     int rScore = 12;
@@ -33,7 +33,7 @@ public class VolleyballTest {
     assertEquals(0.046377890909090946, volleyball.win(sScore, rScore, probWinServe), DELTA * 0.046377890909090946);
   }
 
-  //@Test
+  @Test
   public void case4() {
     int sScore = 9;
     int rScore = 6;
@@ -41,7 +41,7 @@ public class VolleyballTest {
     assertEquals(0.8207606501003635, volleyball.win(sScore, rScore, probWinServe), DELTA * 0.8207606501003635);
   }
 
-  //@Test
+  @Test
   public void case5() {
     int sScore = 13;
     int rScore = 11;
@@ -57,7 +57,7 @@ public class VolleyballTest {
     assertEquals(0.45454545454545453, volleyball.win(sScore, rScore, probWinServe), DELTA * 0.45454545454545453);
   }
 
-  //@Test
+  @Test
   public void case7() {
     int sScore = 13;
     int rScore = 14;
@@ -65,7 +65,7 @@ public class VolleyballTest {
     assertEquals(0.18181818181818182, volleyball.win(sScore, rScore, probWinServe), DELTA * 0.18181818181818182);
   }
 
-  //@Test
+  @Test
   public void case8() {
     int sScore = 19;
     int rScore = 18;
@@ -73,7 +73,7 @@ public class VolleyballTest {
     assertEquals(0.6923076923076923, volleyball.win(sScore, rScore, probWinServe), DELTA * 0.6923076923076923);
   }
 
-  //@Test
+  @Test
   public void case9() {
     int sScore = 4;
     int rScore = 3;
@@ -81,7 +81,7 @@ public class VolleyballTest {
     assertEquals(0.9058476778308387, volleyball.win(sScore, rScore, probWinServe), DELTA * 0.9058476778308387);
   }
 
-  //@Test
+  @Test
   public void case10() {
     int sScore = 4;
     int rScore = 3;
@@ -89,7 +89,7 @@ public class VolleyballTest {
     assertEquals(0.6662085066547871, volleyball.win(sScore, rScore, probWinServe), DELTA * 0.6662085066547871);
   }
 
-  //@Test
+  @Test
   public void case11() {
     int sScore = 0;
     int rScore = 0;
@@ -97,7 +97,7 @@ public class VolleyballTest {
     assertEquals(0.5092217353167382, volleyball.win(sScore, rScore, probWinServe), DELTA * 0.5092217353167382);
   }
 
-  //@Test
+  @Test
   public void case12() {
     int sScore = 85;
     int rScore = 84;
@@ -105,7 +105,7 @@ public class VolleyballTest {
     assertEquals(0.6799856011519078, volleyball.win(sScore, rScore, probWinServe), DELTA * 0.6799856011519078);
   }
 
-  //@Test
+  @Test
   public void case13() {
     int sScore = 85;
     int rScore = 85;
@@ -113,7 +113,7 @@ public class VolleyballTest {
     assertEquals(0.3599712023038157, volleyball.win(sScore, rScore, probWinServe), DELTA * 0.3599712023038157);
   }
 
-  //@Test
+  @Test
   public void case14() {
     int sScore = 14;
     int rScore = 0;
@@ -121,7 +121,7 @@ public class VolleyballTest {
     assertEquals(0.9999999999999678, volleyball.win(sScore, rScore, probWinServe), DELTA * 0.9999999999999678);
   }
 
-  //@Test
+  @Test
   public void case15() {
     int sScore = 1;
     int rScore = 14;
@@ -129,7 +129,7 @@ public class VolleyballTest {
     assertEquals(0.8603147284303606, volleyball.win(sScore, rScore, probWinServe), DELTA * 0.8603147284303606);
   }
 
-  //@Test
+  @Test
   public void case16() {
     int sScore = 7;
     int rScore = 2;
@@ -137,7 +137,7 @@ public class VolleyballTest {
     assertEquals(0.9286294531682486, volleyball.win(sScore, rScore, probWinServe), DELTA * 0.9286294531682486);
   }
 
-  //@Test
+  @Test
   public void case17() {
     int sScore = 100;
     int rScore = 100;
@@ -145,7 +145,7 @@ public class VolleyballTest {
     assertEquals(0.33557046979865773, volleyball.win(sScore, rScore, probWinServe), DELTA * 0.33557046979865773);
   }
 
-  //@Test
+  @Test
   public void case18() {
     int sScore = 15;
     int rScore = 15;
@@ -153,7 +153,7 @@ public class VolleyballTest {
     assertEquals(0.8333333333333334, volleyball.win(sScore, rScore, probWinServe), DELTA * 0.8333333333333334);
   }
 
-  //@Test
+  @Test
   public void case19() {
     int sScore = 100;
     int rScore = 99;
@@ -161,7 +161,7 @@ public class VolleyballTest {
     assertEquals(0.6968503937007875, volleyball.win(sScore, rScore, probWinServe), DELTA * 0.6968503937007875);
   }
 
-  //@Test
+  @Test
   public void case20() {
     int sScore = 100;
     int rScore = 99;
