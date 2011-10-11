6ab0502395aa1a48047468746d0cc69c711cba5e
gabriel <gabesoft@gmail.com>
2011-10-10 19:40:27 -0700

    volleyball - cleanup

--- src/math/Volleyball.java
+++ src/math/Volleyball.java
@@ -8,13 +8,13 @@ import java.io.*;
 // statement: http://community.topcoder.com/stat?c=problem_statement&pm=2959&rd=5880
 // editorial: http://www.topcoder.com/tc?module=Static&d1=match_editorials&d2=tco04_online_rd_3
 public class Volleyball {
-  int LIM = 5000;
+  int LIM = 2500;
   double[][][] memo;
   double sProb;
   double rProb;
 
   public double win(int sScore, int rScore, double probWinServe) {
-    memo  = new double[2][LIM / 2 + 10][LIM / 2 + 10];
+    memo  = new double[2][LIM + 1][LIM + 1];
 
     sProb = probWinServe;
     rProb = 1.0 - sProb;
@@ -28,14 +28,12 @@ public class Volleyball {
     if (win(r, s)) { return 1 - sTeam; }
     if (win(s, r)) { return sTeam; }
 
-    if (s + r > LIM) { return 0.0; }
+    if (s >= LIM || r >= LIM) { return 0.0; }
 
-    double res = 0.0;
-    res += sProb * prob(s + 1, r, sTeam);
-    res += rProb * prob(r + 1, s, 1 - sTeam);
+    memo[sTeam][s][r] += sProb * prob(s + 1, r, sTeam);
+    memo[sTeam][s][r] += rProb * prob(r + 1, s, 1 - sTeam);
 
-    memo[sTeam][s][r] = res;
-    return res;
+    return memo[sTeam][s][r];
   }
 
   boolean win(int aScore, int bScore) {
