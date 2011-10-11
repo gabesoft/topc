7830ed48729bff2ecf6ade1f12530fd1480fefb5
gabriel <gabesoft@gmail.com>
2011-10-10 19:45:20 -0700

    volleyball - speedup

--- src/math/Volleyball.java
+++ src/math/Volleyball.java
@@ -8,32 +8,32 @@ import java.io.*;
 // statement: http://community.topcoder.com/stat?c=problem_statement&pm=2959&rd=5880
 // editorial: http://www.topcoder.com/tc?module=Static&d1=match_editorials&d2=tco04_online_rd_3
 public class Volleyball {
-  int LIM = 2500;
-  double[][][] memo;
+  int LIM = 1000;
+  double[][] memo;
   double sProb;
   double rProb;
 
   public double win(int sScore, int rScore, double probWinServe) {
-    memo  = new double[2][LIM + 1][LIM + 1];
+    memo  = new double[LIM + 1][LIM + 1];
 
     sProb = probWinServe;
     rProb = 1.0 - sProb;
 
-    return prob(sScore, rScore, 1);
+    return prob(sScore, rScore);
   }
 
-  double prob(int s, int r, int sTeam) {
-    if (memo[sTeam][s][r] > 0.0) { return memo[sTeam][s][r]; }
+  double prob(int s, int r) {
+    if (memo[s][r] > 0.0) { return memo[s][r]; }
 
-    if (win(r, s)) { return 1 - sTeam; }
-    if (win(s, r)) { return sTeam; }
+    if (win(r, s)) { return 0; }
+    if (win(s, r)) { return 1; }
 
     if (s >= LIM || r >= LIM) { return 0.0; }
 
-    memo[sTeam][s][r] += sProb * prob(s + 1, r, sTeam);
-    memo[sTeam][s][r] += rProb * prob(r + 1, s, 1 - sTeam);
+    memo[s][r] += sProb * prob(s + 1, r);
+    memo[s][r] += rProb * (1 - prob(r + 1, s));
 
-    return memo[sTeam][s][r];
+    return memo[s][r];
   }
 
   boolean win(int aScore, int bScore) {
