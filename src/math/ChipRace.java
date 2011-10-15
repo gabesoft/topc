89b6c96440696bc0d92f1793922ffb212ea81a2a
gabriel <gabesoft@gmail.com>
2011-10-14 19:33:09 -0700

    chiprace - complete

--- src/math/ChipRace.java
+++ src/math/ChipRace.java
@@ -8,101 +8,40 @@ import java.io.*;
 // statement: http://community.topcoder.com/stat?c=problem_statement&pm=2436&rd=5074
 // editorial: http://www.topcoder.com/tc?module=Static&d1=match_editorials&d2=srm199
 public class ChipRace {
-  int N; // number of players
-  int C; // number of chips to be awarded
-  int S; // sum of odd chips
-  int[] chips;
-  int[][] subsets;
-  double[][] prob;
-
   public double[] chances(int[] chips) {
-    this.chips = chips;
-
-    N = chips.length;
-    S = arraySum(chips);
-    C = (int)Math.round(S / 5.0);
-    prob = new double[C][N];
-
-    buildSubsets();
-
-      for (int i = 0; i < N; i++) {
-        prob[0][i] = (double)chips[i] / S;
-      }
-
-    for (int i = 1; i < C; i++) {
-      for (int j = 0; j < N; j++) {
-        calcProb(i, j);
-      }
-    }
-
-    // TODO: remove debug code
-    debug(C, S, N);
-    //for (int i = 0; i < C; i++) {
-    //debug(prob[i]);
-    //}
-    //for (int i = 0; i < N; i++) {
-      //debug(i, subsets[i]);
-    //}
-    // -----------------------
-
-    double[] res = new double[N];
-    for (int i = 0; i < C; i++) {
-      for (int j = 0; j < N; j++) {
-        res[j] += prob[i][j];
-      }
-    }
-
-    debug("res", res);
-    return res;
+    int s = arraySum(chips);
+    int c = (int)Math.round(s / 5.0);
+    return rec(chips, c);
   }
 
-  void calcProb(int c, int k) {
-    //for (int i = 0; i < N; i++) {
-      //if (i == k) { continue; }
-      //prob[c][k] += (prob[c - 1][i] * chips[k]) / (S - chips[i]);
-    //}
+  double[] rec(int[] chips, int c) {
+    int n = chips.length;
+    double prob[] = new double[n];
 
-    int[] sets = subsets[c - 1];
-    debug(c, k, sets);
+    if (c == 0) { return prob; }
 
-    for (int i = 0; i < sets.length; i++) {
-      if (sets[i] == 0) { break; }
-      if (((sets[i] >> k) & 1) == 1) { continue; };
-      int idx = 0;
-      double p = 1.0;
-      double s = S;
-      debug(i, "sets[i]", sets[i]);
-      for (int j = 0; j < N; j++) {
-        if (((sets[i] >> j) & 1) == 0) { continue; }
-        // TODO: need to iterate through all perms here for p
-        p *= prob[idx++][j];
-        s -= chips[j];
-        debug("idx", idx, "j", j, s);
+    int s = arraySum(chips);
+    for (int i = 0; i < n; i++) {
+      if (chips[i] == 0) { continue; }
+
+      double p = (double)chips[i] / s;
+      prob[i] += p;
+
+      int[] next = chips.clone();
+      next[i] = 0;
+
+      double[] prec = rec(next, c - 1);
+      for (int j = 0; j < n; j++) {
+        prob[j] += p * prec[j];
       }
-      prob[c][k] += p * (chips[k] / s);
     }
-    debug("prob[c][k]", c, k, prob[c][k]);
-  }
 
-  void buildSubsets() {
-    subsets = new int[N][1 << N];
-    int[] idx = new int[N];
-    for (int i = 1; i < 1 << N; i++) {
-      int j = countBits(i);
-      int k = idx[j - 1]++;
-      subsets[j - 1][k] = i;
-    }
+    return prob;
   }
 
   int arraySum(int[] arr) {
     int sum = 0;
-    for (int i = 0; i < N; i++) { sum += chips[i]; }
-    return sum;
-  }
-
-  int countBits(int n) {
-    int sum = n;
-    while (n != 0) { n = n >> 1; sum -= n; }
+    for (int i = 0; i < arr.length; i++) { sum += arr[i]; }
     return sum;
   }
 
--- test/math/ChipRaceTest.java
+++ test/math/ChipRaceTest.java
@@ -9,175 +9,175 @@ public class ChipRaceTest {
   double DELTA = 1.0e-09;
   ChipRace chiprace = new ChipRace();
 
-  //@Test
+  @Test
   public void case1() {
     int[] chips = { 1, 1 };
     assertArrayEquals(new double[] { 0.0, 0.0 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case2() {
     int[] chips = { 1, 2 };
     assertArrayEquals(new double[] { 0.3333333333333333, 0.6666666666666666 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case3() {
     int[] chips = { 3, 2, 3 };
     assertArrayEquals(new double[] { 0.725, 0.55, 0.725 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case4() {
     int[] chips = { 0, 1, 2, 3, 4 };
     assertArrayEquals(new double[] { 0.0, 0.23452380952380952, 0.4412698412698413, 0.6083333333333334, 0.7158730158730159 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case5() {
     int[] chips = { 0, 1, 1, 0, 0, 0, 0, 1, 1, 0 };
     assertArrayEquals(new double[] { 0.0, 0.25, 0.25, 0.0, 0.0, 0.0, 0.0, 0.25, 0.25, 0.0 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case6() {
     int[] chips = { 0 };
     assertArrayEquals(new double[] { 0.0 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case7() {
     int[] chips = { 1 };
     assertArrayEquals(new double[] { 0.0 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case8() {
     int[] chips = { 2 };
     assertArrayEquals(new double[] { 0.0 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case9() {
     int[] chips = { 3 };
     assertArrayEquals(new double[] { 1.0 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case10() {
     int[] chips = { 4 };
     assertArrayEquals(new double[] { 1.0 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case11() {
     int[] chips = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     assertArrayEquals(new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case12() {
     int[] chips = { 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 };
     assertArrayEquals(new double[] { 0.7999999999999999, 0.7999999999999999, 0.7999999999999999, 0.7999999999999999, 0.7999999999999999, 0.7999999999999999, 0.7999999999999999, 0.7999999999999999, 0.7999999999999999, 0.7999999999999999 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case13() {
     int[] chips = { 4, 4, 4, 4, 4, 4, 4, 4, 4, 3 };
     assertArrayEquals(new double[] { 0.8098535383838402, 0.8098535383838402, 0.8098535383838402, 0.8098535383838402, 0.8098535383838402, 0.8098535383838402, 0.8098535383838402, 0.8098535383838402, 0.8098535383838402, 0.7113181545454373 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case14() {
     int[] chips = { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4 };
     assertArrayEquals(new double[] { 0.7113181545454373, 0.8098535383838402, 0.8098535383838402, 0.8098535383838402, 0.8098535383838402, 0.8098535383838402, 0.8098535383838402, 0.8098535383838402, 0.8098535383838402, 0.8098535383838402 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case15() {
     int[] chips = { 2, 0, 1, 4, 3, 2, 3, 4, 1, 2 };
     assertArrayEquals(new double[] { 0.3975378165006648, 0.0, 0.2174042503763866, 0.6467565114623939, 0.5395325134102223, 0.3975378165006648, 0.5395325134102223, 0.6467565114623939, 0.2174042503763866, 0.3975378165006648 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case16() {
     int[] chips = { 4, 0, 2, 4, 1, 2, 4, 2, 3, 4 };
     assertArrayEquals(new double[] { 0.7030558798226049, 0.0, 0.44743931678916204, 0.7030558798226049, 0.24918667055563, 0.44743931678916204, 0.7030558798226049, 0.44743931678916204, 0.5962718597864648, 0.7030558798226049 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case17() {
     int[] chips = { 0, 1, 0, 0, 0, 0, 0, 0, 0 };
     assertArrayEquals(new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case18() {
     int[] chips = { 0, 0, 1, 0, 0, 1, 0, 0, 0 };
     assertArrayEquals(new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case19() {
     int[] chips = { 0, 0, 0, 1, 1, 0, 1, 0, 0 };
     assertArrayEquals(new double[] { 0.0, 0.0, 0.0, 0.3333333333333333, 0.3333333333333333, 0.0, 0.3333333333333333, 0.0, 0.0 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case20() {
     int[] chips = { 0, 0, 0, 0, 0, 0, 0, 2, 0 };
     assertArrayEquals(new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case21() {
     int[] chips = { 0, 0, 0, 0, 0, 1, 0, 1, 1 };
     assertArrayEquals(new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.3333333333333333, 0.0, 0.3333333333333333, 0.3333333333333333 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case22() {
     int[] chips = { 2, 2, 2, 2, 2, 2, 2, 3, 2, 2 };
     assertArrayEquals(new double[] { 0.3861713106295151, 0.3861713106295151, 0.3861713106295151, 0.3861713106295151, 0.3861713106295151, 0.3861713106295151, 0.3861713106295151, 0.5244582043343653, 0.3861713106295151, 0.3861713106295151 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case23() {
     int[] chips = { 2, 3, 4, 4, 4, 4, 4, 4, 4, 4 };
     assertArrayEquals(new double[] { 0.47856455442662343, 0.630282831995987, 0.7363940766971738, 0.7363940766971738, 0.7363940766971738, 0.7363940766971738, 0.7363940766971738, 0.7363940766971738, 0.7363940766971738, 0.7363940766971738 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case24() {
     int[] chips = { 4, 4, 4, 4, 4, 4, 4, 4, 4, 4 };
     assertArrayEquals(new double[] { 0.7999999999999999, 0.7999999999999999, 0.7999999999999999, 0.7999999999999999, 0.7999999999999999, 0.7999999999999999, 0.7999999999999999, 0.7999999999999999, 0.7999999999999999, 0.7999999999999999 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case25() {
     int[] chips = { 3, 4, 4, 2 };
     assertArrayEquals(new double[] { 0.7485181485181485, 0.8377067377067378, 0.8377067377067378, 0.576068376068376 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case26() {
     int[] chips = { 1, 1 };
     assertArrayEquals(new double[] { 0.0, 0.0 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case27() {
     int[] chips = { 4, 4, 4, 4, 4, 4, 4, 4 };
     assertArrayEquals(new double[] { 0.7500000000000001, 0.7500000000000001, 0.7500000000000001, 0.7500000000000001, 0.7500000000000001, 0.7500000000000001, 0.7500000000000001, 0.7500000000000001 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case28() {
     int[] chips = { 4, 4, 4, 4, 4, 4, 3, 3, 4, 4 };
     assertArrayEquals(new double[] { 0.819316754203792, 0.819316754203792, 0.819316754203792, 0.819316754203792, 0.819316754203792, 0.819316754203792, 0.7227329831848304, 0.7227329831848304, 0.819316754203792, 0.819316754203792 }, chiprace.chances(chips), DELTA);
   }
 
-  //@Test
+  @Test
   public void case29() {
     int[] chips = { 4, 3, 2, 1, 0, 1, 2, 3, 4, 4 };
     assertArrayEquals(new double[] { 0.739294914116559, 0.6351605813309945, 0.4833672168306709, 0.272529830663496, 0.0, 0.272529830663496, 0.4833672168306709, 0.6351605813309945, 0.739294914116559, 0.739294914116559 }, chiprace.chances(chips), DELTA);
