681c171d6d13bf5d387d31d8e84475939456c261
gabriel <gabesoft@gmail.com>
2011-10-20 21:38:59 -0700

    packageshipping - stubs

---
+++ src/dynamic/PackageShipping.java
@@ -0,0 +1,18 @@
+package topc.dynamic;
+
+import java.util.*;
+import java.io.*;
+
+// TCCC05 Round 3 Division I Level Two - 500
+// dynamic programming, graph theory
+// statement: http://community.topcoder.com/stat?c=problem_statement&pm=3481&rd=6530
+// editorial: http://www.topcoder.com/tc?module=Static&d1=match_editorials&d2=tccc05_online_rd_3
+public class PackageShipping {
+  public double ship(String[] routes, String origin, String destination, int time, int packageCost) {
+    return 0.0;
+  }
+
+  private void debug(Object... os) {
+    System.out.println(Arrays.deepToString(os));
+  }
+}
--- test/AllTests.java
+++ test/AllTests.java
@@ -4,78 +4,9 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import topc.test.easy.*;
-import topc.test.graph.*;
-import topc.test.bkt.*;
 import topc.test.dynamic.*;
-import topc.test.greedy.*;
-import topc.test.stringm.*;
-import topc.test.math.*;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({
-  //BusinessTasksTest.class,
-  //TallPeopleTest.class,
-  //TableSeatingTest.class,
-  //MedalTableTest.class,
-  //SmartWordToyTest.class,
-  //RevolvingDoorsTest.class,
-  //BridgeCrossingTest.class,
-  //ZigZagTest.class,
-  //BadNeighborsTest.class,
-  //FlowerGardenTest.class,
-  //AvoidRoadsTest.class,
-  //JewelryTest.class,
-  //StripePainterTest.class,
-  //FibTest.class,
-  //WeightedIntervalSchedulingTest.class,
-  //QuickSumsTest.class,
-  //ShortPalindromesTest.class,
-  //GameOfLifeDivOneTest.class,
-  //GrafixMaskTest.class,
-  //MarketingTest.class,
-  //CircuitsTest.class,
-  //PathFindingTest.class,
-  //EscapeTest.class,
-  //KiloManXTest.class,
-  //KiloManX2Test.class,
-  //KiloManX3Test.class,
-  //RoboCourierTest.class,
-  //IslandFerriesTest.class,
-  //DungeonEscapeTest.class,
-  //TeamBuilderTest.class,
-  //ParkingTest.class,
-  //StarAdventureTest.class,
-  //MiniPaintTest.class,
-  //MiniPaint2Test.class,
-  //MNSTest.class,
-  //WeirdRooksTest.class,
-  //PoetryTest.class,
-  //BioScoreTest.class,
-  //GoldMineTest.class,
-  //WorldPeaceTest.class,
-  //MLBRecordTest.class,
-  //RearrangeFurnitureTest.class,
-  //HeatDeathTest.class,
-  //RationalizationTest.class,
-  //BenfordsLawTest.class,
-  //QuizShowTest.class,
-  //BirthdayOddsTest.class,
-  //CollisionTest.class,
-  //NestedRandomnessTest.class,
-  //ChessKnightTest.class,
-  //DiceThrowsTest.class,
-  //RockSkippingTest.class,
-  //PointSystemTest.class,
-  //VolleyballTest.class,
-  //GeneticCrossoverTest.class,
-  //ProbabilityTreeTest.class,
-  //ChipRaceTest.class,
-  //TopFiveTest.class,
-  //OneArmedBanditTest.class,
-  //RangeGameTest.class,
-  //YahtzeeRollTest.class,
-  //BagOfDevouringTest.class,
-  RandomFATest.class
-})
-public class AllTests { }
+@Suite.SuiteClasses({ PackageShippingTest.class })
+public class AllTests {
+}
