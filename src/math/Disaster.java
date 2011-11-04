120d0e173857e565e016ac5a3dc8db979ed8305b
gabriel <gabesoft@gmail.com>
2011-11-03 18:58:59 -0700

    disaster - cleanup

--- src/math/Disaster.java
+++ src/math/Disaster.java
@@ -8,9 +8,6 @@ import java.io.*;
 // statement: http://community.topcoder.com/stat?c=problem_statement&pm=3563&rd=6550
 // editorial: http://www.topcoder.com/tc?module=Static&d1=tournaments&d2=tccc05&d3=alg_room1_analysis
 public class Disaster {
-  static public final double T = 3.0e-8;      // accuracy tolerance
-  static public final double R = 0.61803399;
-  static public final double C = 1.0 - R;
   static public final int S = 0;              // start village
   static public final int E = 1;              // end village
   int N;
@@ -30,50 +27,6 @@ public class Disaster {
     }
 
     return findBestRoad().maxe;
-  }
-
-  double findMax(int exp) {
-    double xa = 0.0;
-    double xc = 1.0 / B;
-    double xb = 0.5 * (xa + xc);
-
-    if (exp == 0) { return xc; }
-
-    assert f(xb, exp) > f(xa, exp) : "malfunction: xa " + exp;
-    assert f(xb, exp) > f(xc, exp) : "malfunction: xc " + exp;
-
-    double[] x = new double[4];
-    x[0] = xa;
-    x[3] = xc;
-
-    if (Math.abs(xc - xb) > Math.abs(xb - xa)) {
-      x[1] = xb;
-      x[2] = xb + C * (xc - xb);
-    } else {
-      x[1] = xb - C * (xb - xa);
-      x[2] = xb;
-    }
-
-    double[] y = new double[2];
-    y[0] = f(x[1], exp);
-    y[1] = f(x[2], exp);
-    while (Math.abs(x[3] - x[0]) > T * (x[1] + x[2])) {
-      if (y[1] > y[0]) {
-        x[0] = x[1];
-        x[1] = x[2];
-        x[2] = R * x[2] + C * x[3];
-        y[0] = y[1];
-        y[1] = f(x[2], exp);
-      } else {
-        x[3] = x[2];
-        x[2] = x[1];
-        x[1] = R * x[1] + C * x[0];
-        y[1] = y[0];
-        y[0] = f(x[1], exp);
-      }
-    }
-
-    return y[0] > y[1] ? y[0] : y[1];
   }
 
   double f(double v, int exp) {
@@ -120,7 +73,7 @@ public class Disaster {
       this.prob = prob;
       this.coef = coef;
       this.village = village;
-      this.maxe = findMax(coef) * prob;
+      this.maxe = prob * f(1.0 / (B + B * coef), coef);
     }
 
     public int compareTo(Node other) {
