b37562d4da55d120156ddc5cc22c120d2e5416ce
gabriel <gabesoft@gmail.com>
2011-11-06 09:18:06 -0800

    primepolynom - complete

--- src/easy/PrimePolynom.java
+++ src/easy/PrimePolynom.java
@@ -9,7 +9,24 @@ import java.io.*;
 // editorial: http://www.topcoder.com/tc?module=Static&d1=match_editorials&d2=srm259
 public class PrimePolynom {
   public int reveal(int A, int B, int C) {
-    return 0;
+    int m = 0;
+    while (true) {
+      int v = A * m * m + B * m + C;
+      if (!isPrime(v)) { return m; };
+      m++;
+    }
+  }
+
+  boolean isPrime(int n){
+    if (n < 2)      { return false; }
+    if (n == 2)     { return true;  }
+    if (n % 2 == 0) { return false; }
+
+    for (int i = 3; i * i <= n; i += 2) {
+      if (n % i == 0) { return false; }
+    }
+
+    return true;
   }
 
   private void debug(Object... os) {
