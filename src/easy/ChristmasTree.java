2578d4891e6d48099b3335a754bf1cad87a975f7
gabriel <gabesoft@gmail.com>
2011-12-04 19:45:54 -0800

    christmastree - cleanup

--- src/easy/ChristmasTree.java
+++ src/easy/ChristmasTree.java
@@ -8,67 +8,37 @@ import java.io.*;
 // statement: http://community.topcoder.com/stat?c=problem_statement&pm=7261&rd=10011
 // editorial: http://www.topcoder.com/tc?module=Static&d1=match_editorials&d2=srm331
 public class ChristmasTree {
-  final int[][] W = new int[][] { new int[] { 0 },
-    new int[] { 1 },
-    new int[] { 1, 2 },
-    new int[] { 1, 3 },
-    new int[] { 1, 2, 4 },
-    new int[] { 1, 5 },
-    new int[] { 1, 2, 3, 6 },
-    new int[] { 1, 7 },
-    new int[] { 1, 2, 4, 8 },
-    new int[] { 1, 3, 9 },
-    new int[] { 1, 2, 5, 10 },
-  };
-
   public long decorationWays(int N, int red, int green, int blue) {
-    return ways(N, new int[] { red, green, blue });
+    return ways(N, red, green, blue);
   }
 
-  long ways(int n, int[] rgb) {
+  long ways(int n, int r, int g, int b) {
+    if (r < 0 || g < 0 || b < 0) { return 0; }
+    if (n == 0) { return 1; }
+
     long sum = 0;
-    for (int i = 0; i < W[n].length; i++) {
-      sum += ways(n, W[n][i], rgb);
+
+    sum += level(n, 1) * ways(n - 1, r - n, g, b);
+    sum += level(n, 1) * ways(n - 1, r, g - n, b);
+    sum += level(n, 1) * ways(n - 1, r, g, b - n);
+
+    if (n % 2 == 0) {
+      int k = n / 2;
+      sum += level(n, 2) * ways(n - 1, r - k, g - k, b);
+      sum += level(n, 2) * ways(n - 1, r - k, g, b - k);
+      sum += level(n, 2) * ways(n - 1, r, g - k, b - k);
     }
+
+    if (n % 3 == 0) {
+      int k = n / 3;
+      sum += level(n, 3) * ways(n - 1, r - k, g - k, b - k);
+    }
+
     return sum;
   }
 
-  long ways(int n, int k, int[] rgb) {
-    if (k == 0) { return 1; }
-
-    long sum = 0;
-    int c = n / k;
-
-    if (c == 1) {
-      for (int i = 0; i < rgb.length; i++) {
-        if (rgb[i] >= k) {
-          int[] next = rgb.clone();
-          next[i] -= k;
-          sum += ways(n - 1, next);
-        }
-      }
-    }
-    else if (c == 2) {
-      for (int i = 0; i < rgb.length; i++) {
-        for (int j = i + 1; j < rgb.length; j++) {
-          if (i != j && rgb[i] >= k && rgb[j] >= k) {
-            int[] next = rgb.clone();
-            next[i] -= k;
-            next[j] -= k;
-            sum += ways(n - 1, next);
-          }
-        }
-      }
-    }
-    else if (c == 3) {
-      if (rgb[0] >= k && rgb[1] >= k && rgb[2] >= k) {
-        int[] next = rgb.clone();
-        for (int i = 0; i < rgb.length; i++) { next[i] -= k; }
-        sum += ways(n - 1, next);
-      }
-    }
-
-    return fact(n) / (long)Math.pow(fact(k), c) * sum;
+  long level(int n, int c) {
+    return fact(n) / (long)Math.pow(fact(n / c), c);
   }
 
   long fact(int n) {
