c1ea2579ed1138d1ca525bdc550dc7bffa3d4751
gabriel <gabesoft@gmail.com>
2011-12-09 20:27:11 -0800

    floatingmedian - alternate solution

--- src/sorting/FloatingMedian.java
+++ src/sorting/FloatingMedian.java
@@ -9,53 +9,56 @@ import java.io.*;
 // editorial: http://www.topcoder.com/tc?module=Static&d1=match_editorials&d2=srm310
 public class FloatingMedian {
   int M = 65536;
+  int R = 17;
+  int[][] tree;
 
   public long sumOfMedians(int seed, int mul, int add, int N, int K) {
     long[] nums = new long[N + 1];
 
+    tree = new int[R][M];
     nums[0] = (int)seed;
     for (int i = 1; i < N + 1; i++) {
       nums[i] = (nums[i - 1] * mul + add) % M;
     }
 
-    long[] sorted = Arrays.copyOfRange(nums, 0, K);
-    Arrays.sort(sorted);
-
-    long sum = median(sorted);
-    for (int i = K; i < N; i++) {
-      resort(sorted, nums[i - K], nums[i]);
-      sum += median(sorted);
+    long sum = 0L;
+    for (int i = 0; i < N; i++) {
+      insert((int)nums[i]);
+      if (i >= K) {
+        remove((int)nums[i - K]);
+      }
+      if (i >= K - 1) {
+        sum += kth((K + 1) / 2);
+      }
     }
 
     return sum;
   }
 
-  void resort(long[] input, long rem, long add) {
-    if (add == rem) { return; }
-
-    int k = Arrays.binarySearch(input, 0, input.length, rem);
-
-    if (add < rem) {
-      for (int i = k; i > -1; i--) {
-        if (i == 0 || input[i - 1] < add) {
-          input[i] = add;
-          break;
-        } else { input[i] = input[i - 1]; }
-      }
-    } else {
-      for (int i = k; i < input.length; i++) {
-        if (i == input.length - 1 || input[i + 1] > add) {
-          input[i] = add;
-          break;
-        } else { input[i] = input[i + 1]; }
-      }
+  void insert(int x) {
+    for (int i = 0; i < R; i++) {
+      tree[i][x]++;
+      x /= 2;
     }
   }
 
-  long median(long[] input) {
-    int l = input.length;
-    int k = (l + 1) / 2;
-    return input[k - 1];
+  void remove(int x) {
+    for (int i = 0; i < R; i++) {
+      tree[i][x]--;
+      x /= 2;
+    }
+  }
+
+  long kth(int k) {
+    int a = 0;
+    for(int b = 16; b >= 0; b--) {
+      a *= 2;
+      if (tree[b][a] < k) {
+        k -= tree[b][a];
+        a++;
+      }
+    }
+    return a;
   }
 
   private void debug(Object... os) {
