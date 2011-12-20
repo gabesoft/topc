f8c4fe99bdeb3758c0a09ea83aad11fa9d2b4991
gabriel <gabesoft@gmail.com>
2011-12-19 20:10:01 -0800

    remoterover - complete

--- src/geometry/RemoteRover.java
+++ src/geometry/RemoteRover.java
@@ -8,126 +8,69 @@ import java.io.*;
 // statement: http://community.topcoder.com/stat?c=problem_statement&pm=4022&rd=6534
 // editorial: http://www.topcoder.com/tc?module=Static&d1=match_editorials&d2=srm235
 public class RemoteRover {
-  static public final double R = 0.61803399;
-  static public final double C = 1.0 - R;
   int[] width;
   int[] speed;
-  double[][] memo;
   int n;
 
   public double optimalTravel(int[] width, int[] speed, int offset) {
     this.n = width.length;
     this.width = width;
     this.speed = speed;
-    this.memo = new double[n + 1][offset + 1];
 
-    //debug(n, width, speed);
-    return calcRaw(0, offset);
+    double[] theta = calcTheta(offset);
+    double time = calcTime(theta);
 
-    //return calc(0, offset);
+    return time;
   }
 
-  double calcRaw(int index, int offset) {
-    if (memo[index][offset] > 0.0) { return memo[index][offset]; }
-    if (index  == n - 1) { return time(index, offset); }
-    if (offset == 0.0)   { return time(index, offset) + calcRaw(index + 1, offset); }
+  double[] calcTheta(double offset) {
+    double[] theta = new double[n];
 
-    double best = Double.MAX_VALUE / 2.0;
-    int lo = 0;
-    int hi = offset;
-    while (lo < hi) {
-      int mid = (hi + lo) / 2;
-      double cur = time(index, mid) + calcRaw(index + 1, offset - mid);
+    if (offset == 0) { return theta; }
 
-      if (cur < best) {
-        best = cur;
-        hi = mid;
-      } else {
-        lo = mid + 1;
-      }
-    }
-
-    memo[index][offset] = best;
-    return best;
-  }
-
-  double calc(int index, double offset) {
-    if (index  == n - 1) { return time(index, offset); }
-    if (offset == 0.0)   { return time(index, offset) + calc(index + 1, offset); }
-
-    //double raw = memo[index][(int)offset];
-
-    double er = 7.0e-4;
-
-    double ax = 0.0;
-    double cx = offset;
-    double bx = ax + (cx - ax) / 2.0;
-
-    double x0 = ax;
-    double x1;
-    double x2;
-    double x3 = cx;
-
-    if (Math.abs(cx - bx) > Math.abs(bx - ax)) {
-      x1 = bx;
-      x2 = bx + C * (cx - bx);
-    } else {
-      x2 = bx;
-      x1 = bx - C * (bx - ax);
-    }
-
-    double f1 = func(x1, offset, index);
-    double f2 = func(x2, offset, index);
+    double lo = 0.0;
+    double hi = 90.0;
 
     int count = 0;
-    while (Math.abs(x3 - x0) > er * (Math.abs(x1) + Math.abs(x2))) {
-      count++;
-      if (f2 < f1) {
-        x0 = x1;
-        x1 = x2;
-        x2 = R * x2 + C * x3;
-        f1 = f2;
-        f2 = func(x2, offset, index);
+    while (count++ < 100) {
+      double mid = lo + (hi - lo) / 2.0;
+
+      theta[0] = sin(mid);
+      for (int i = 1; i < n; i++) {
+        theta[i] = theta[i - 1] * (speed[i] / (double)speed[i - 1]);
+      }
+
+      if (calcOffset(theta) > offset) {
+        hi = mid;
       } else {
-        x3 = x2;
-        x2 = x1;
-        x1 = R * x1 + C * x0;
-        f2 = f1;
-        f1 = func(x1, offset, index);
+        lo = mid;
       }
     }
 
-    return f1 < f2 ? f1 : f2;
-
-    //double best = Double.MAX_VALUE / 2.0;
-    //double lo = 0.0;
-    //double hi = offset;
-    //while (true) {
-    //double mid = lo + (hi - lo) / 2.0;
-    //double cur = time(index, mid) + calc(index + 1, offset - mid);
-
-    //if (Math.abs(cur - best) < 1.0e-12) { break; }
-
-    //if (cur < best) {
-    //best = cur;
-    //hi = mid;
-    //} else {
-    //lo = mid;
-    //}
-    //}
-
-    //return best;
+    return theta;
   }
 
-  double func(double x, double offset, int index) {
-    assert x > 0.0    : "negative x";
-    assert x < offset : "x greater than offset";
-    return time(index, x) + calc(index + 1, offset - x);
+  double calcTime(double[] theta) {
+    double sum = 0.0;
+    for (int i = 0; i < n; i++) {
+      double ts = theta[i] * theta[i];
+      double ws = width[i] * width[i];
+      double dx = (ts * ws) / (1.0 - ts);
+      sum += Math.sqrt(dx + ws) / speed[i];
+    }
+    return sum;
   }
 
-  double time(int index, double offset) {
-    double dist = Math.sqrt(offset * offset + width[index] * width[index]);
-    return dist / speed[index];
+  double calcOffset(double[] theta) {
+    double sum = 0.0;
+    for (int i = 0; i < n; i++) {
+      sum += theta[i] * width[i] / Math.sqrt(1.0 - theta[i] * theta[i]);
+    }
+    return Double.isNaN(sum) ? Double.MAX_VALUE / 2.0 : sum;
+  }
+
+  double sin(double deg) {
+    return Math.sin(Math.toRadians(deg));
   }
 
   private void debug(Object... os) {
