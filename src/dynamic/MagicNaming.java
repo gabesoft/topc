f9923ae83bfbf0f9aeeb67d883e38c471e955b9f
gabriel <gabesoft@gmail.com>
2012-01-27 21:22:31 -0800

    magicnaming - refactoring

--- src/dynamic/MagicNaming.java
+++ src/dynamic/MagicNaming.java
@@ -8,37 +8,36 @@ import java.io.*;
 // statement: http://community.topcoder.com/stat?c=problem_statement&pm=11674&rd=14762
 // editorial: http://apps.topcoder.com/wiki/display/tc/SRM+526.5
 public class MagicNaming {
-  HashMap<String, Integer> cache;
+  int[][] cache;
+  String name;
 
   public int maxReindeers(String magicName) {
-    cache = new HashMap<String, Integer>();
-    return getCount(magicName, "");
+    name  = magicName;
+    cache = new int[magicName.length()][magicName.length()];
+    return getCount(0, 0);
   }
 
-  int getCount(String s, String prev) {
-    if (s.length() == 0) { return 0; }
+  int getCount(int index, int prev) {
+    if (index == name.length()) { return 0; }
+    if (cache[index][prev] > 0) { return cache[index][prev]; }
 
-    String key = s + prev;
-    if (cache.containsKey(key)) { return cache.get(key); }
+    String p = name.substring(prev, index);
+    String s = name.substring(index, name.length());
 
-    String s1 = "";
-    String s2 = "";
     int best = 1;
-    int curr = 0;
-    for (int i = 1; i < s.length(); i++) {
-      if (s.charAt(i) < s.charAt(0)) { continue; }
-      s1 = s.substring(0, i);
-      s2 = s.substring(i, s.length());
-      boolean c2 = s.compareTo(s2 + s1) <= 0;
-      boolean c1 = c2 && (prev.compareTo(s1) <= 0 || (prev + s1).compareTo(s1 + prev) < 0);
+    for (int i = index + 1; i < name.length(); i++) {
+      if (name.charAt(i) < name.charAt(index)) { continue; }
 
-      if (c1 && c2) {
-        curr = 1 + getCount(s2, s1);
-        best = Math.max(best, curr);
-      }
+      String s1 = name.substring(index, i);
+      String s2 = name.substring(i, name.length());
+
+      boolean c1 = p.compareTo(s1) <= 0 || (p + s1).compareTo(s1 + p) < 0;
+      boolean c2 = s.compareTo(s2 + s1) <= 0;
+
+      if (c1 && c2) { best = Math.max(best, 1 + getCount(i, index)); }
     }
 
-    cache.put(key, best);
+    cache[index][prev] = best;
     return best;
   }
 
