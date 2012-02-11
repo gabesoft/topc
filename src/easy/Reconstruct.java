655b5dfdcf3c3640e7a94680be9088e998fe4124
gabriel <gabesoft@gmail.com>
2012-02-11 08:46:55 -0800

    reconstruct - cleanup

--- src/easy/Reconstruct.java
+++ src/easy/Reconstruct.java
@@ -9,142 +9,77 @@ import java.io.*;
 // editorial: http://www.topcoder.com/tc?module=Static&d1=match_editorials&d2=srm218
 public class Reconstruct {
   int n;
-  int[][] dist;
-  int[][][] triple;
-  int[][] points;
+  int[][] dst;
+  int[][] pts;
 
   public String[] findPoints(String[] dists) {
     n = dists.length;
-    dist = new int[n][n];
+    dst = new int[n][n];
+
     for (int i = 0; i < n; i++) {
       String[] nums = dists[i].split("\\s+");
       for (int j = 0; j < nums.length; j++) {
-        dist[i][j] = Integer.parseInt(nums[j]);
+        dst[i][j] = Integer.parseInt(nums[j]);
       }
     }
 
-    triple = new int[1001][72][];
-    int[] counts = new int[1001];
-    for (int i = 0; i < 32; i++) {
-      for (int j = 0; j < 32; j++) {
-        for (int k = 0; k < 32; k++) {
-          int v = i * i + j * j + k * k;
-          if (v <= 1000) {
-            int[] t = { i, j, k };
-            triple[v][counts[v]++] = t;
+    this.pts = null;
+    int[][] pts = new int[n][];
+
+    pts[0] = new int[] { 0, 0, 0 };
+
+    for (int x = 0; x < 32; x++) {
+      for (int y = 0; y < 32; y++) {
+        for (int z = 0; z < 32; z++) {
+
+          if (find(pts, new int[] { x, y, z }, 1)) {
+            String[] res = new String[n];
+            for (int i = 0; i < n; i++) {
+              int[] pt = this.pts[i];
+              res[i] = String.format("%s %s %s", pt[0], pt[1], pt[2]);
+            }
+            return res;
           }
+
         }
       }
     }
 
-    this.points = null;
-    int[][] points = new int[n][];
-    int d = dist[1][0];
-
-    points[0] = new int[] { 0, 0, 0 };
-    for (int i = 0; i < triple[d].length; i++) {
-      if (triple[d][i] == null) { break; }
-      points[1] = triple[d][i];
-      if (find(points, 2)) { break; }
-    }
-
-    if (this.points == null) {
-      return new String[] { };
-    }
-
-    String[] res = new String[n];
-    for (int i = 0; i < n; i++) {
-      int[] point = this.points[i];
-      res[i] = String.format("%s %s %s", point[0], point[1], point[2]);
-    }
-
-    return res;
+    return new String[] { };
   }
 
-  boolean find(int[][] points, int next) {
+  boolean find(int[][] pts, int[] pt, int next) {
     if (next == n) {
-      this.points = points;
+      this.pts = pts;
       return true;
     }
 
-    int[][] trial = getPoints(dist[next][0]);
-    for (int i = 0; i < trial.length; i++) {
-      int[] point = trial[i];
-      boolean ok = true;
+    for (int i = 0; i < next; i++) {
+      if (dist(pts[i], pt) != dst[next][i]) { return false; };
+    }
 
-      for (int j = 1; j < next; j++) {
-        if (!hasDist(points[j], point, dist[next][j])) {
-          ok = false;
-          break;
+    pts[next] = pt;
+
+    for (int x = -31; x < 32; x++) {
+      for (int y = -31; y < 32; y++) {
+        for (int z = -31; z < 32; z++) {
+          if (find(pts, new int[] { x, y, z }, next + 1)) { return true; }
         }
-      }
-
-      if (ok) {
-        points[next] = point;
-        if (find(points, next + 1)) { return true; }
       }
     }
 
     return false;
   }
 
-  int[][] getPoints(int dist) {
-    List<Point> points = new ArrayList<Point>();
-
-    for (int i = 0; i < triple[dist].length; i++) {
-      if (triple[dist][i] == null) { break; }
-      int[] vals = triple[dist][i];
-      for (int a = -1; a < 2; a += 2) {
-        for (int b = -1; b < 2; b += 2) {
-          for (int c = -1; c < 2; c += 2) {
-            points.add(new Point(a * vals[0], b * vals[1], c * vals[2]));
-          }
-        }
-      }
-    }
-
-    Collections.sort(points);
-    int[][] res = new int[points.size()][];
-
-    for (int i = 0; i < res.length; i++) {
-      res[i] = points.get(i).toArray();
-    }
-
-    return res;
-  }
-
-  boolean hasDist(int[] p1, int[] p2, int dist) {
+  int dist(int[] p1, int[] p2) {
     int d = 0;
     for (int i = 0; i < p1.length; i++) {
       d += (p1[i] - p2[i]) * (p1[i] - p2[i]);
     }
-    return d == dist;
+    return d;
   }
 
   private void debug(Object... os) {
     System.out.println(Arrays.deepToString(os));
-  }
-
-  class Point implements Comparable<Point> {
-    public final int x;
-    public final int y;
-    public final int z;
-
-    public Point(int x, int y, int z) {
-      this.x = x;
-      this.y = y;
-      this.z = z;
-    }
-
-    public int[] toArray() {
-      return new int[] { x, y, z };
-    }
-
-    public int compareTo(Point other) {
-      if (x != other.x) { return Integer.valueOf(x).compareTo(other.x); }
-      if (y != other.y) { return Integer.valueOf(y).compareTo(other.y); }
-      if (z != other.z) { return Integer.valueOf(z).compareTo(other.z); }
-      return 0;
-    }
   }
 }
