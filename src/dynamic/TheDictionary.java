f92990366c1f87832328302ebdd2f9e88ac551e7
Gabriel Adomnicai <gabesoft@gmail.com>
2012-06-16 18:49:11 -0700

    thedictionary - refactoring

--- src/dynamic/TheDictionary.java
+++ src/dynamic/TheDictionary.java
@@ -9,51 +9,45 @@ import java.math.*;
 // statement: http://community.topcoder.com/stat?c=problem_statement&pm=10183&rd=13519
 // editorial: http://www.topcoder.com/tc?module=Static&d1=match_editorials&d2=srm428
 public class TheDictionary {
-     BigInteger [][][] cnt;
+    int MAX = 1000000001;
+    int [][][] cnt;
 
     public String find(int n, int m, int k) {
-        cnt  = new BigInteger[2][n + 1][m + 1];
-
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < m + 1; j++) {
-                cnt[0][i][j] = BigInteger.valueOf(0);
-                cnt[1][i][j] = BigInteger.valueOf(0);
-            }
-        }
+        cnt  = new int[2][n + 1][m + 1];
 
         for (int i = 1; i < n + 1; i++) {
-            cnt[0][i][0] = BigInteger.valueOf(1);
+            cnt[0][i][0] = 1;
         }
 
         for (int j = 1; j < m + 1; j++) {
-            cnt[1][0][j] = BigInteger.valueOf(1);
+            cnt[1][0][j] = 1;
         }
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < m + 1; j++) {
-                cnt[0][i][j] = cnt[0][i - 1][j].add(cnt[1][i - 1][j]);
-                cnt[1][i][j] = cnt[0][i][j - 1].add(cnt[1][i][j - 1]);
+                cnt[0][i][j] = Math.min(cnt[0][i - 1][j] + cnt[1][i - 1][j], MAX);
+                cnt[1][i][j] = Math.min(cnt[0][i][j - 1] + cnt[1][i][j - 1], MAX);
             }
         }
 
-        String res = rec(n, m, BigInteger.valueOf(k));
+        String res = rec(n, m, k);
         return res.indexOf("-1") > -1 ? "" : res;
     }
 
-    String rec(int n, int m, BigInteger k) {
+    String rec(int n, int m, int k) {
         if (n == 0 && m == 0) { return ""; }
         if (n < 0 || m < 0) { return "-1"; }
-        if (k.longValue() < 0) { return "-1"; }
+        if (k < 0) { return "-1"; }
 
-        BigInteger acnt = cnt[0][n][m];
-        BigInteger zcnt = cnt[1][n][m];
+        int acnt = cnt[0][n][m];
+        int zcnt = cnt[1][n][m];
 
-        if (k.compareTo(acnt) <= 0) {
+        if (k <= acnt) {
             return "a" + rec(n - 1, m, k);
         }
 
-        if (k.compareTo(acnt.add(zcnt)) <= 0) {
-            return "z" + rec(n, m - 1, k.subtract(acnt));
+        if (k <= acnt + zcnt) {
+            return "z" + rec(n, m - 1, k - acnt);
         }
 
         return "-1";
