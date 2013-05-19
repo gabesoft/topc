83437ce2f62fc5224333b22f14978defc1d43937
Gabriel Adomnicai <gabesoft@gmail.com>
2013-05-18 19:49:05 -0700

    circledance - refactoring

--- src/greedy/CircleDance.java
+++ src/greedy/CircleDance.java
@@ -10,51 +10,15 @@ import java.io.*;
 public class CircleDance {
     public int arrangeDancers(int[] heights) {
         int n = heights.length;
-        boolean seen[] = new boolean[n];
-
-        int l = 0;
-        int r = 0;
         int d = 0;
 
-        for (int i = 1; i < n; i++) {
-            if (heights[i] > heights[l]) {
-                l = i;
-                r = i;
-            }
+        Arrays.sort(heights);
+
+        for (int i = 2; i < n; i++) {
+            d = Math.max(d, heights[i] - heights[i - 2]);
         }
 
-        seen[l] = true;
-        int c   = 1;
-
-        while (c < n) {
-            int k = -1;
-
-            for (int i = 0; i < n; i++) {
-                if (seen[i]) { continue; }
-
-                if (k == -1) {
-                    k = i;
-                } else if (heights[i] > heights[k]) {
-                    k = i;
-                }
-            }
-
-            if (l == r) {
-                d = Math.max(d, heights[l] - heights[k]);
-                l = k;
-            } else if (heights[l] > heights[r]) {
-                d = Math.max(d, heights[l] - heights[k]);
-                l = k;
-            } else {
-                d = Math.max(d, heights[r] - heights[k]);
-                r = k;
-            }
-
-            seen[k] = true;
-            c++;
-        }
-
-        return Math.max(d, Math.abs(heights[l] - heights[r]));
+        return d;
     }
 
     private void debug(Object... os) {
