5c7ed3aa64ca493db2ff7fc0803b0ad585338b58
Gabriel Adomnicai <gabesoft@gmail.com>
2013-07-15 15:34:41 -0700

    hexagonalsums - alternate solution

--- src/search/HexagonalSums.java
+++ src/search/HexagonalSums.java
@@ -9,14 +9,36 @@ import java.io.*;
 // editorial: http://www.topcoder.com/tc?module=Static&d1=match_editorials&d2=srm205
 public class HexagonalSums {
     int INF = 1 << 20;
+    int[] H;
     public int minLength(int N) {
-        int[] H = buildHexTable(N);
-        int[] M = new int[N + 1];
+        H = buildHexTable(N);
+        //return sol1(N);
+        return sol2(N); // faster
+    }
+
+    private int sol2(int n) {
+        for (int i = 0; i < H.length; i++) {
+            if (H[i] == n) { return 1; }
+        }
+
+        if (search(n, 2, 0)) { return 2; }
+        if (n > 146858) { return 3; }
+        if (search(n, 3, 0)) { return 3; }
+        if (n > 130) { return 4; }
+        if (search(n, 4, 0)) { return 4; }
+        if (n > 26) { return 5; }
+        if (search(n, 5, 0)) { return 5; }
+
+        return 6;
+    }
+
+    private int sol1(int n) {
+        int[] M = new int[n + 1];
 
         Arrays.fill(M, INF);
         for (int i = 0; i < H.length; i++) {
             int h = H[i];
-            if (h <= N) {
+            if (h <= n) {
                 M[h] = 1;
             }
         }
@@ -24,7 +46,7 @@ public class HexagonalSums {
         boolean change = true;
         while (change) {
             change = false;
-            if (M[N] < INF) { break; }
+            if (M[n] < INF) { break; }
             for (int i = 0; i < H.length; i++) {
                 int h = H[i];
                 for (int j = 0; j + h < M.length; j++) {
@@ -36,7 +58,25 @@ public class HexagonalSums {
             }
         }
 
-        return M[N];
+        return M[n];
+    }
+
+    private boolean search(int n, int terms, int best) {
+        if (n < 0) { return false; }
+        if (terms > 2) {
+            for (int i = best; H[i] <= n / terms; i++) {
+                if (search(n - H[i], terms - 1, i)) {
+                    return true;
+                }
+            }
+        } else {
+            for (int i = 0; H[i] <= n / terms; i++) {
+                if (Arrays.binarySearch(H, n - H[i]) >= 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     private int[] buildHexTable(int n) {
