c6a58cf49b208d5a4a9418e515e946e4e12a5001
Gabriel Adomnicai <gabesoft@gmail.com>
2013-07-16 18:52:11 -0700

    simplepath - cleanup

--- src/geometry/SimplePath.java
+++ src/geometry/SimplePath.java
@@ -9,18 +9,15 @@ import java.io.*;
 // editorial: http://www.topcoder.com/tc?module=Static&d1=match_editorials&d2=srm202
 public class SimplePath {
     public int trouble(String direction, int[] length) {
-        int n = length.length;
-
-        Segment seg[] = new Segment[length.length];
+        int n  = length.length;
         int sx = 0;
         int sy = 0;
 
+        Segment seg[] = new Segment[length.length];
         for (int i = 0; i < n; i++) {
-            char d  = direction.charAt(i);
-            int len = length[i];
-            seg[i]  = new Segment(sx, sy, d, len, i);
-            sx      = seg[i].ex;
-            sy      = seg[i].ey;
+            seg[i] = new Segment(sx, sy, direction.charAt(i), length[i], i);
+            sx     = seg[i].ex;
+            sy     = seg[i].ey;
         }
 
         for (int i = 0; i < n; i++) {
@@ -39,19 +36,16 @@ public class SimplePath {
     }
 
     public class Segment {
-        boolean vert;
         int pos;
-
         public final int sx;
         public final int sy;
         public final int ex;
         public final int ey;
 
         public Segment(int sx, int sy, char dir, int len, int pos) {
-            this.sx   = sx;
-            this.sy   = sy;
-            this.vert = (dir == 'N' || dir == 'S');
-            this.pos  = pos;
+            this.sx  = sx;
+            this.sy  = sy;
+            this.pos = pos;
 
             switch (dir) {
                 case 'N': 
@@ -74,50 +68,27 @@ public class SimplePath {
                     ex = 0;
                     ey = 0;
             }
-
-
         }
 
         private boolean intersects(Segment s) {
-            Segment s1 = s.pos > pos ? this : s;
-            Segment s2 = s.pos > pos ? s : this;
+            int x1 = Math.min(sx, ex);
+            int y1 = Math.min(sy, ey);
+            int x2 = Math.max(sx, ex);
+            int y2 = Math.max(sy, ey);
 
-            if (s1.ex == s2.ex && s1.ey == s2.ey) {
-                return true;
-            }
-            if (s1.vert && s2.vert) {
-                return s1.sx == s2.sx && intersects(s1.sy, s1.ey, s2.sy, s2.ey);
-            }
-            if (!s1.vert && !s2.vert) {
-                return s1.sy == s2.sy && intersects(s1.sx, s1.ex, s2.sx, s2.ex);
-            }
+            int x3 = Math.min(s.sx, s.ex);
+            int y3 = Math.min(s.sy, s.ey);
+            int x4 = Math.max(s.sx, s.ex);
+            int y4 = Math.max(s.sy, s.ey);
 
-            int x  = s1.vert ? s1.sx : s2.sx;
-            int y  = s1.vert ? s2.sy : s1.sy;
-            int x1 = s1.vert ? s2.sx : s1.sx;
-            int x2 = s1.vert ? s2.ex : s1.ex;
-            int y1 = s1.vert ? s1.sy : s2.sy;
-            int y2 = s1.vert ? s1.ey : s2.ey;
-
-            if (intersects(x1, x2, x) && intersects(y1, y2, y)) {
-                return s1.pos + 1 < s2.pos;
+            if (Math.abs(pos - s.pos) == 1) {
+                boolean a = x3 < x2 && x1 < x4 && y3 < y2 && y1 < y4;
+                boolean b = y1 == y3 && x3 < x2 && x1 < x4;
+                boolean c = x1 == x3 && y3 < y2 && y1 < y4;
+                return a || b || c;
             } else {
-                return false;
+                return x3 <= x2 && x1 <= x4 && y3 <= y2 && y1 <= y4;
             }
-        }
-
-        private boolean intersects(int s1, int s2, int s) {
-            int a = Math.min(s1, s2);
-            int b = Math.max(s1, s2);
-            return a <= s && s <= b;
-        }
-
-        private boolean intersects(int s1, int s2, int s3, int s4) {
-            int a = Math.min(s1, s2);
-            int b = Math.max(s1, s2);
-            int x = Math.min(s3, s4);
-            int y = Math.max(s3, s4);
-            return x < b && a < y;
         }
 
         public String toString() {
