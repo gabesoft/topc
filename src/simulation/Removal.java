6b4cc6a6e682d49b65e519c93acce9761cc1ebf1
Gabriel Adomnicai <gabesoft@gmail.com>
2013-07-27 11:14:36 -0700

    removal - cleanup

--- src/simulation/Removal.java
+++ src/simulation/Removal.java
@@ -9,60 +9,23 @@ import java.io.*;
 // editorial: http://www.topcoder.com/tc?module=Static&d1=match_editorials&d2=srm177
 public class Removal {
     public int finalPos(int n, int k, String[] remove) {
-        Adj[] adj = new Adj[remove.length];
-        int items = n;
+        long r = k;
 
-        for (int i = 0; i < remove.length; i++) {
+        for (int i = remove.length - 1; i > -1; i--) {
             String[] range = remove[i].split("-");
 
             int lo = Integer.parseInt(range[0]);
             int hi = Integer.parseInt(range[1]);
-            int v  = hi - lo + 1;
 
-            adj[i] = new Adj(lo, v);
-            items -= v;
-
-            for (int j = 0; j < i; j++) {
-                if (adj[j].k >= adj[i].k && adj[j].k <= hi) {
-                    adj[j].move(adj[j].k - adj[i].k);
-                } else if (adj[j].k >= adj[i].k) {
-                    adj[j].move(v);
-                }
+            if (r >= lo) {
+                r += (hi - lo + 1);
             }
         }
 
-        if (k > items) { return -1; }
-
-        int r = k;
-        for (int i = 0; i < adj.length; i++) {
-            if (k >= adj[i].k) {
-                r += adj[i].v;
-            }
-        }
-
-        return r;
+        return r > n ? -1 : (int)r;
     }
 
     private void debug(Object... os) {
         System.out.println(Arrays.deepToString(os));
     }
-
-    public class Adj {
-        public int k;
-        public int v;
-
-        public Adj(int threshold, int adjustment) {
-            k = threshold;
-            v = adjustment;
-        }
-
-        public void move(int d) {
-            k -= d;
-        }
-
-        public String toString() {
-            return k + ":" + v;
-        }
-    }
-
 }
