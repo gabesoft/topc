42c5ef05a73b71a16363899491cef051846d8a09
Gabriel Adomnicai <gabesoft@gmail.com>
2013-08-06 11:43:35 -0700

    hourglass - alternate solution

--- src/easy/HourGlass.java
+++ src/easy/HourGlass.java
@@ -11,13 +11,19 @@ public class HourGlass {
     int A;
     int B;
     HashSet<Integer> set;
+    boolean[][][] seen;
 
     public int[] measurable(int glass1, int glass2) {
-        A = Math.min(glass1, glass2);
-        B = Math.max(glass1, glass2);
+        A    = Math.min(glass1, glass2);
+        B    = Math.max(glass1, glass2);
+        set  = new HashSet<Integer>();
+        seen = new boolean[A + 1][B + 1][501];
 
-        set = new HashSet<Integer>();
+        return iterative();   // faster
+        //return recursive();
+    }
 
+    private int[] iterative() {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 for (int k = 0; k < 10; k++) {
@@ -26,7 +32,57 @@ public class HourGlass {
                 }
             }
         }
+        return getValues();
+    }
 
+    private int[] recursive() {
+        rec(0, 0, 0);
+        return getValues();
+    }
+
+    private void rec(int x, int y, int t) {
+        if (t > 500) { return; }
+        if (seen[x][y][t]) { return; }
+
+        seen[x][y][t] = true;
+        set.add(t);
+
+        if (x == 0 && y == 0) {
+            rec(0, A, t + A);
+            rec(0, 0, t + A + B);
+        }
+        if (y == 0) {
+            // flip
+            rec(0, x, t + x);
+            // don't flip
+            rec(0, A - x, t + (A - x));
+        }
+        if (x == 0) {
+            // flip 
+            if (y < A) {
+                rec(y, 0, t + y);
+            }
+            if (y == A) {
+                rec(0, 0, t + y);
+            }
+            if (y > A) {
+                rec(0, y - A, t + A);
+            }
+
+            // don't flip
+            if (B - y < A) {
+                rec(B - y, 0, t + (B - y));
+            }
+            if (B - y == A) {
+                rec(0, 0, t + (B - y));
+            }
+            if (B - y > A) {
+                rec(0, y + A, t + A);
+            }
+        }
+    }
+
+    private int[] getValues() {
         int[] values = new int[set.size()];
         int i = 0;
         for (int k : set) {
