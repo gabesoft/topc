2728ec022e1d7865b2fd42b98ad1ec4a536551d9
Gabriel Adomnicai <gabesoft@gmail.com>
2013-08-22 16:41:45 -0700

    arcademanao - complete

---
+++ src/graph/ArcadeManao.java
@@ -0,0 +1,90 @@
+package topc.graph;
+
+import java.util.*;
+import java.io.*;
+
+// SRM 576 Division I Level One - 256
+// graph theory, simple search, iteration
+// statement: http://community.topcoder.com/stat?c=problem_statement&pm=12504&rd=15496
+// editorial: http://apps.topcoder.com/wiki/display/tc/SRM+576
+public class ArcadeManao {
+    int INF = 1 << 30;
+
+    public int shortestLadder(String[] level, int coinRow, int coinColumn) {
+        char grid[][] = new char[level.length][level[0].length()];
+
+        char k    = '0';
+        char last = '.';
+        int coin  = -1;
+        int start = -1;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (i == coinRow - 1 && j == coinColumn - 1) {
+                    coin = k - '0';
+                }
+
+                char c = level[i].charAt(j);
+                if (c == 'X') {
+                    grid[i][j] = k;
+                    start = k - '0';
+                }
+                if (c == '.') {
+                    grid[i][j] = c;
+                    if (last == 'X') { k++; }
+                }
+                last = c;
+            }
+
+            if (last == 'X') { k++; }
+        }
+
+        int n = (int)(k - '0') + 1;
+        int graph[][] = new int[n][n];
+
+        for (int i1 = 0; i1 < grid.length; i1++) {
+            for (int j1 = 0; j1 < grid[0].length; j1++) {
+                for (int i2 = i1 + 1; i2 < grid.length; i2++) {
+                    for (int j2 = 0; j2 < grid[0].length; j2++) {
+                        if (j1 == j2 && grid[i1][j1] != '.' && grid[i2][j2] != '.') {
+                            int u = (int)(grid[i1][j1] - '0');
+                            int v = (int)(grid[i2][j2] - '0');
+
+                            graph[u][v] = i2 - i1;
+                            graph[v][u] = i2 - i1;
+                        }
+                    }
+                }
+            }
+        }
+
+        int[][] seen = new int[n][grid.length];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(seen[i], -1);
+        }
+
+        return dfs(graph, seen, start, coin, 0);
+    }
+
+    private int dfs(int[][] graph, int[][] seen, int start, int end, int len) {
+        if (start == end) { return len; }
+        if (seen[start][len] == -2) return INF;
+        if (seen[start][len] > -1) return seen[start][len];
+
+        seen[start][len] = -2;
+
+        int best = INF;
+        for (int i = 0; i < graph.length; i++) {
+            if (graph[start][i] > 0) {
+                best = Math.min(best, dfs(graph, seen, i, end, Math.max(graph[start][i], len)));
+            }
+        }
+
+        seen[start][len] = best;
+        return best;
+    }
+
+    private void debug(Object... os) {
+        System.out.println(Arrays.deepToString(os));
+    }
+}
