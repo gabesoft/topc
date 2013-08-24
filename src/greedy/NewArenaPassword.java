33cc630e9dae0a9c00f42273ec29060d5c333156
Gabriel Adomnicai <gabesoft@gmail.com>
2013-08-24 15:13:58 -0700

    newarenapassword - cleanup

--- src/greedy/NewArenaPassword.java
+++ src/greedy/NewArenaPassword.java
@@ -8,38 +8,37 @@ import java.io.*;
 // statement: http://community.topcoder.com/stat?c=problem_statement&pm=12386&rd=15492
 // editorial: http://apps.topcoder.com/wiki/display/tc/SRM+572
 public class NewArenaPassword {
+    int L = 26;
+
     public int minChange(String oldPassword, int K) {
-        int n = oldPassword.length(); 
+        int n = oldPassword.length();
 
         if (K == n) { return 0; }
 
-        if (K < n / 2) {
-            int count = 0;
-            for (int i = 0; i < K; i++) {
-                if (oldPassword.charAt(i) != oldPassword.charAt(n - K + i)) {
-                    count++;
-                }
-            }
-            return count;
-        } else {
-            int count = 0;
-            int p = n - K;
+        int count = 0;
+        int p = n - K;
 
-            for (int i = 0; i < p; i++) {
-                int chars[] = new int[26];
-                int all = 0;
-                for (int j = i; j < n; j += p) {
-                    char c = oldPassword.charAt(j);
-                    chars[(int)(c - 'a')]++;
-                    all++;
-                }
-
-                Arrays.sort(chars);
-                count += (all - chars[25]);
+        for (int i = 0; i < p && i < K; i++) {
+            int chars[] = new int[L];
+            int all = 0;
+            for (int j = i; j < n; j += p) {
+                char c = oldPassword.charAt(j);
+                chars[c - 'a']++;
+                all++;
             }
 
-            return count;
+            count += (all - max(chars));
         }
+
+        return count;
+    }
+
+    private int max(int[] a) {
+        int mx = a[0];
+        for (int i = 1; i < a.length; i++) {
+            mx = Math.max(mx, a[i]);
+        }
+        return mx;
     }
 
     private void debug(Object... os) {
