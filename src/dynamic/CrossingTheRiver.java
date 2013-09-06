a23ca29e35bf3d86d73b69e8373df834e182dd04
Gabriel Adomnicai <gabesoft@gmail.com>
2013-09-05 20:26:00 -0700

    crossingtheriver - alternate solution

--- src/dynamic/CrossingTheRiver.java
+++ src/dynamic/CrossingTheRiver.java
@@ -15,7 +15,83 @@ public class CrossingTheRiver {
     boolean[] taken;
 
     public String isItEvenPossible(int waterWidth, int landWidth, int[] blockHeight, int depth) {
-        this.n       = blockHeight.length;
+        this.n = blockHeight.length;
+
+        //return solve1(waterWidth, landWidth, blockHeight, depth);
+        return solve2(waterWidth, landWidth, blockHeight, depth);
+    }
+
+    private String solve2(int waterWidth, int landWidth, int[] blockHeight, int depth) {
+        int[] count = new int[101];
+
+        for (int i = 0; i < n; i++) {
+            count[blockHeight[i]]++;
+        }
+
+        if (count[depth] >= waterWidth) {
+            return "POSSIBLE";
+        }
+
+        for (int wl = depth; wl <= depth + 1; wl++) {
+            if (count[wl] == 0) { continue; }
+
+            for (int wh = wl; wh <= 100; wh++) {
+                if (count[wh] == 0) { continue; }
+
+                for (int ll = wh - depth; ll <= wh - depth + 1; ll++) {
+                    if (ll < 0 || count[ll] == 0) { continue; }
+
+                    for (int lh = ll; lh <= 100; lh++) {
+                        if (count[lh] == 0) { continue; }
+
+                        if (possiblePick(waterWidth, landWidth, count.clone(), depth, wl, wh, ll, lh)) {
+                            return "POSSIBLE";
+                        }
+                    }
+                }
+            }
+        }
+
+        return "IMPOSSIBLE";
+    }
+
+    private boolean possiblePick(int waterWidth, int landWidth, int[] rem, int depth, int wl, int wh, int ll, int lh) {
+        for (int x = wl; x <= wh; x++) {
+            if ((--waterWidth < 0) || (--rem[x] < 0)) {
+                return false;
+            }
+        }
+
+        for (int x = ll; x <= lh; x++) {
+            if ((--landWidth < 0) || (--rem[x] < 0)) {
+                return false;
+            }
+        }
+
+        int inter = 0;
+        int landonly = 0;
+        int wateronly = 0;
+
+        for (int x = depth; x <= 100; x++) {
+            boolean water = (wl <= x && x <= wh);
+            boolean land  = (ll <= x && x <= lh);
+
+            if (water && land) {
+                inter += rem[x];
+            } else if (water) {
+                wateronly += rem[x];
+            } else if (land) {
+                landonly += rem[x];
+            }
+        }
+
+        waterWidth = Math.max(0, waterWidth - wateronly);
+        landWidth  = Math.max(0, landWidth - landonly);
+
+        return waterWidth + landWidth <= inter;
+    }
+
+    private String solve1(int waterWidth, int landWidth, int[] blockHeight, int depth) {
         this.heights = blockHeight;
         this.depth   = depth;
         this.taken   = new boolean[n];
