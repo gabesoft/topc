2f27455acf876be8581b43ef73494e9f33d2eed3
Gabriel Adomnicai <gabesoft@gmail.com>
2013-09-26 08:30:00 -0700

    ellysjuice - alternate solution

--- src/search/EllysJuice.java
+++ src/search/EllysJuice.java
@@ -8,88 +8,26 @@ import java.io.*;
 // statement: http://community.topcoder.com/stat?c=problem_statement&pm=11843&rd=15090
 // editorial: http://apps.topcoder.com/wiki/display/tc/TCO+2012+Round+1a
 public class EllysJuice {
-    int n;
-    int[] perm;
-    int level;
-    String[] players;
-    HashSet<String> winners;
-
     public String[] getWinners(String[] players) {
-        n = players.length;
+        int n = players.length;
+        if (n == 1) { return players; }
 
-        this.players = players;
-        this.winners = new HashSet<String>();
-        this.perm    = new int[n];
-        this.level   = -1;
+        Arrays.sort(players);
 
-        permute(0);
-
-        String r[] = new String[winners.size()];
-        int k = 0;
-
-        for (String winner : winners) {
-            r[k++] = winner;
-        }
-
-        Arrays.sort(r);
-
-        return r;
-    }
-
-    private String getWinner(int[] perm) {
-        int drinks[] = new int[] { 1 << (n + 1), 1 << (n + 1) };
-
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
-        for (int i = 0; i < n; i++) {
-            int k = perm[i] - 1;
-            String name = players[k];
-
-            drinks[i % 2] /= 2;
-            int amt = drinks[i % 2];
-
-            if (!map.containsKey(name)) {
-                map.put(name, 0);
-            }
-            map.put(name, map.get(name) + amt);
-        }
-
-        String winner = "";
-        int best = 0;
-        for (String name : map.keySet()) {
-            if (map.get(name) > best) {
-                best = map.get(name);
-                winner = name;
-            }
-        }
-        for (String name : map.keySet()) {
-            if (name != winner && map.get(name) == best) {
-                return null;
-            }
-        }
-
-        return winner;
-    }
-
-    private void permute(int k) {
-        level++;
-        perm[k] = level;
-        if (level == perm.length) {
-            String winner = getWinner(perm);
-            if (winner != null) {
-                winners.add(winner);
-            }
-        } else {
-            for (int i = 0; i < perm.length; i++) {
-                if (perm[i] == 0) {
-                    permute(i);
+        int len = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (players[i].compareTo(players[i + 1]) == 0) {
+                if (len == 0 || players[len - 1].compareTo(players[i]) != 0) {
+                    players[len++] = players[i];
                 }
             }
         }
-        level--;
-        perm[k] = 0;
-    }
 
-    private void debug(Object... os) {
-        System.out.println(Arrays.deepToString(os));
+        String[] r = new String[len];
+        for (int i = 0; i < len; i++) {
+            r[i] = players[i];
+        }
+
+        return r;
     }
 }
