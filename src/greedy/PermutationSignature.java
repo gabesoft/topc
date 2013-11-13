48664eb0e5bd88df1bb487322e775e84fbbd3b3f
Gabriel Adomnicai <gabesoft@gmail.com>
2013-11-13 08:46:17 -0800

    permutationsignature - cleanup

--- src/greedy/PermutationSignature.java
+++ src/greedy/PermutationSignature.java
@@ -8,68 +8,19 @@ import java.io.*;
 // statement: http://community.topcoder.com/stat?c=problem_statement&pm=11115&rd=14426
 // editorial: http://apps.topcoder.com/wiki/display/tc/SRM+497
 public class PermutationSignature {
-    String sig;
-    int n;
-
     public int[] reconstruct(String signature) {
-        n = signature.length();
-        sig = signature;
+        int n = signature.length() + 1;
 
-        int d = 1;
-        int s = 0;
-        while (s < n && signature.charAt(s++) == 'D') {
-            d++;
+        if (n == 1) { return new int[] { 1 }; }
+
+        int[] next = reconstruct(signature.substring(1));
+        int[] perm = new int[n];
+
+        perm[0] = signature.charAt(0) == 'D' ? (next[0] + 1) : 1;
+        for (int i = 1; i < n; i++) {
+            perm[i] = next[i - 1] + (next[i - 1] >= perm[0] ? 1 : 0);
         }
 
-        int[] res = new int[n + 1];
-        boolean used[] = new boolean[n + 2];
-
-        res[0] = d;
-        used[d] = true;
-
-        find(res, used, 0);
-
-        return res;
-    }
-
-    private void find(int[] data, boolean[] used, int k) {
-        if (k == n) { return; }
-
-        char c = sig.charAt(k);
-        if (k < n - 1 && c == 'I' && sig.charAt(k + 1) == 'D') {
-            int d = data[k];
-            int s = k + 1;
-
-            for (int i = 0; i < used.length; i++) {
-                if (used[i] && i > d) {
-                    d = i;
-                }
-            }
-
-            while (s < n && sig.charAt(s++) == 'D') {
-                d++;
-            }
-
-            data[k + 1] = d + 1;
-            used[d + 1] = true;
-
-            find(data, used, k + 1);
-        } else {
-            int a = c == 'I' ? 1 : -1;
-            int d = data[k] + a;
-
-            while (used[d]) {
-                d += a;
-            }
-
-            data[k + 1] = d;
-            used[d] = true;
-
-            find(data, used, k + 1);
-        }
-    }
-
-    private void debug(Object... os) {
-        System.out.println(Arrays.deepToString(os));
+        return perm;
     }
 }
