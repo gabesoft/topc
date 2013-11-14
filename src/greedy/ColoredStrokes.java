4b95ccb815fc6d3106ede41cc4c209200ac8bdc4
Gabriel Adomnicai <gabesoft@gmail.com>
2013-11-13 19:32:47 -0800

    coloredstrokes - cleanup

--- src/greedy/ColoredStrokes.java
+++ src/greedy/ColoredStrokes.java
@@ -12,97 +12,34 @@ public class ColoredStrokes {
         int n = picture.length;
         int m = picture[0].length();
 
-        int[][] pic = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                pic[i][j] = code(picture[i].charAt(j));
-            }
-        }
-
         int count = 0;
+
         for (int i = 0; i < n; i++) {
+            boolean inside = false;
             for (int j = 0; j < m; j++) {
-                if (pic[i][j] == 2) {
-                    clearRow(pic, i, j);
-                    clearCol(pic, i, j);
-                    count += 2;
+                char c = picture[i].charAt(j);
+                if (c == '.' || c == 'B') {
+                    inside = false;
+                } else if (!inside) {
+                    inside = true;
+                    count++;
                 }
             }
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (pic[i][j] != 0) {
-                    if (pic[i][j] == 1) {
-                        clearRow(pic, i, j);
-                        count++;
-                    } else {
-                        clearCol(pic, i, j);
-                        count++;
-                    }
+
+        for (int j = 0; j < m; j++) {
+            boolean inside = false;
+            for (int i = 0; i < n; i++) {
+                char c = picture[i].charAt(j);
+                if (c == '.' || c == 'R') {
+                    inside = false;
+                } else if (!inside) {
+                    inside = true;
+                    count++;
                 }
             }
         }
 
         return count;
-    }
-
-    private void clearCol(int[][] pic, int r, int c) {
-        pic[r][c] = 0;
-
-        for (int i = r + 1; i < pic.length; i++) {
-            if (pic[i][c] == 2) {
-                pic[i][c] = 1;
-            } else if (pic[i][c] == 3) {
-                pic[i][c] = 0;
-            } else {
-                break;
-            }
-        }
-
-        for (int i = r - 1; i > -1; i--) {
-            if (pic[i][c] == 2) {
-                pic[i][c] = 1;
-            } else if (pic[i][c] == 3) {
-                pic[i][c] = 0;
-            } else {
-                break;
-            }
-        }
-    }
-
-    private void clearRow(int[][] pic, int r, int c) {
-        pic[r][c] = 0;
-
-        for (int j = c + 1; j < pic[0].length; j++) {
-            if (pic[r][j] == 2) {
-                pic[r][j] = 3;
-            } else if (pic[r][j] == 1) {
-                pic[r][j] = 0;
-            } else {
-                break;
-            }
-        }
-
-        for (int j = c - 1; j > -1; j--) {
-            if (pic[r][j] == 2) {
-                pic[r][j] = 3;
-            } else if (pic[r][j] == 1) {
-                pic[r][j] = 0;
-            } else {
-                break;
-            }
-        }
-    }
-
-    private int code(char c) {
-        if (c == 'R') { return 1; }
-        if (c == 'G') { return 2; }
-        if (c == 'B') { return 3; }
-        return 0;
-    }
-
-    private void debug(Object... os) {
-        System.out.println(Arrays.deepToString(os));
     }
 }
