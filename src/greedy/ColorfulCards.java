487a72c5f1b6354631eaf13a9f11c56bd44ba831
Gabriel Adomnicai <gabesoft@gmail.com>
2013-11-13 22:36:27 -0800

    colorfulcards - alternate solution

--- src/greedy/ColorfulCards.java
+++ src/greedy/ColorfulCards.java
@@ -8,12 +8,10 @@ import java.io.*;
 // statement: http://community.topcoder.com/stat?c=problem_statement&pm=11302&rd=14424
 // editorial: http://apps.topcoder.com/wiki/display/tc/SRM+495
 public class ColorfulCards {
-    int memo[][];
-
     public int[] theCards(int N, String colors) {
         int n = colors.length();
 
-        int sieve[] = buildSieve(N);
+        int sieve[] = buildSieve(N + 1);
         int input[] = new int[n];
         int cards[] = new int[n];
 
@@ -21,68 +19,47 @@ public class ColorfulCards {
             input[i] = (colors.charAt(i) == 'R') ? 1 : 0;
         }
 
-        memo = new int[n + 1][N + 1];
-
-        int s = 0;
+        int min[] = new int[n];
+        int max[] = new int[n];
+        int x = 0;
         for (int i = 0; i < n; i++) {
-            int c = 0;
-            for (int j = s + 1; j < sieve.length; j++) {
-                if (input[i] != sieve[j]) { continue; }
-                if (valid(sieve, input, i, j)) {
-                    if (c == 0) {
-                        s = j;
-                    }
-                    c++;
-                }
-                if (c > 1) {
-                    break;
-                }
-            }
-            cards[i] = c > 1 ? -1 : s;
+            do {
+                x++;
+            } while (sieve[x] != input[i]);
+            min[i] = x;
+        }
+
+        int y = N + 1;
+        for (int i = n - 1; i > -1; i--) {
+            do { 
+                y--;
+            } while (sieve[y] != input[i]);
+            max[i] = y;
+        }
+
+        for (int i = 0; i < n; i++) {
+            cards[i] = (min[i] == max[i]) ? min[i] : -1;
         }
 
         return cards;
     }
 
-    private boolean valid(int[] sieve, int[] input, int ix, int sx) {
-        if (ix + 1 == input.length) { return true; }
-        if (sx + 1 == sieve.length) { return false; }
-
-        if (memo[ix][sx] > 0) { return memo[ix][sx] == 1; }
-
-        for (int i = sx + 1; i < sieve.length; i++) {
-            if (input[ix + 1] == sieve[i] && valid(sieve, input, ix + 1, i)) {
-                memo[ix][sx] = 1;
-                return true;
-            }
-        }
-
-        memo[ix][sx] = 2;
-        return false;
-    }
-
     int[] buildSieve(int n) {
         int sieve[] = new int[n + 1];
-        int lim     = (int)Math.sqrt(n + 1);
 
-        for (int i = 0; i < sieve.length; i++) {
-            sieve[i] = i;
-        }
+        Arrays.fill(sieve, 1);
+
+        sieve[1] = 0;
 
         for (int i = 2; i < n + 1; i++) {
-            if (sieve[i] == i) {    // i is prime
-                for (int j = 1; j * i < sieve.length; j++) {
-                    sieve[j * i] = i;
+            if (sieve[i] == 1) {
+                for (int j = i * i; j < n + 1; j += i) {
+                    sieve[j] = 0;
                 }
             }
         }
 
-        int res[] = new int[n + 1];
-        for (int i = 2; i < n + 1; i++) {
-            res[i] = (sieve[i] == i) ? 1 : 0;
-        }
-
-        return res;
+        return sieve;
     }
 
 
