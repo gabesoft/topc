23b35769e9292bc948aea81d137add32121d2e10
Gabriel Adomnicai <gabesoft@gmail.com>
2013-12-12 22:17:49 -0800

    thetrianglebothdivs - alternate solution

--- src/search/TheTriangleBothDivs.java
+++ src/search/TheTriangleBothDivs.java
@@ -11,74 +11,39 @@ public class TheTriangleBothDivs {
     public String fix(String input) {
         String[] parts = input.split("\\s+");
         String[] time  = parts[0].split(":");
-        char op        = parts[1].charAt(3);
-        char gmt       = parts[1].charAt(4);
+        String min     = getMin(time[1]);
+        String hour    = time[0];
 
-        String min  = getTime(time[1]);
-        String hour = time[0];
+        int n = input.length();
+        ArrayList<String> matches = new ArrayList<String>();
 
-        if (valid(hour, gmt, op, 24)) {
-            return String.format("%02d:%s", 0, min);
-        }
-        for (int i = 0; i < 24; i++) {
-            if (valid(hour, gmt, op, i)) {
-                return String.format("%02d:%s", i, min);
+        for (int h = 0; h < 24; h++) {
+            for (int g = -9; g < 10; g++) {
+                String str = String.format("%02d:%s GMT%s", h, min, g < 0 ? g : "+" + g);
+
+                boolean match = true;
+                for (int i = 0; i < n; i++) {
+                    if (str.charAt(i) != input.charAt(i)) {
+                        if (input.charAt(i) != '?') {
+                            match = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (match) {
+                    int hh = (h - g + 24) % 24;
+                    matches.add(String.format("%02d:%s", hh, min));
+                }
             }
         }
 
-        return null;
+        Collections.sort(matches);
+
+        return matches.get(0);
     }
 
-    private boolean valid(String hour, char g, char o, int h) {
-        char h1 = hour.charAt(0);
-        char h2 = hour.charAt(1);
-
-        if (h1 == '?' && h2 == '?') { return true; }
-
-        if (h1 != '?' && h2 != '?') {
-            int hh = Integer.parseInt(hour);
-            return valid(g, o, hh, h);
-        }
-
-        for (int i = 0; i < 10; i++) {
-            int hh    = h1 == '?' ? ((i * 10) + (h2 - '0')) : ((h1 - '0') * 10 + i);
-            int delta = hh - h;
-
-            if (hh >= 24) { continue; }
-
-            if (valid(g, o, hh, h)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private boolean valid(char g, char o, int hh, int h) {
-        int delta = hh - h;
-
-        if (o == '?' && g == '?') {
-            return Math.abs(delta) <= 9;
-        }
-        if (g == '?') {
-            return o == '-' ? (delta < 0 && delta >= -9) : (delta >= 0 && delta <= 9);
-        }
-        if (o == '?') {
-            int o1 = (g - '0');
-            int o2 = -o1;
-            return valid(hh, h, o1) || valid(hh, h, o2);
-        } else {
-            int mul = o == '-' ? -1 : 1;
-            int offset = mul * (g - '0');
-            return valid(hh, h, offset);
-        }
-    }
-
-    private boolean valid(int hh, int h, int offset) {
-        return (hh - offset) % 24 == h || (h + offset) % 24 == hh;
-    }
-
-    private String getTime(String str) {
+    private String getMin(String str) {
         char c1 = str.charAt(0);
         char c2 = str.charAt(1);
         return (c1 == '?' ? '0' : c1) + "" + (c2 == '?' ? '0' : c2);
