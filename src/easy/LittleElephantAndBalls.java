398109e8fe32c2f9c6a41a8cf58cdd0cd0d60151
Gabriel Adomnicai <gabesoft@gmail.com>
2013-12-19 21:00:08 -0800

    littleelephantandballs - alternate solution

--- src/easy/LittleElephantAndBalls.java
+++ src/easy/LittleElephantAndBalls.java
@@ -9,42 +9,18 @@ import java.io.*;
 // editorial: http://apps.topcoder.com/wiki/display/tc/SRM+592
 public class LittleElephantAndBalls {
     public int getNumber(String S) {
-        if (S.length() <= 1) { return 0; }
-        if (S.length() == 2) { return 1; }
-        if (S.length() == 3) { return 3; }
+        HashSet<Character> left = new HashSet<Character>();
+        HashSet<Character> right = new HashSet<Character>();
+        char[] chars = S.toCharArray();
 
-        int n       = S.length();
-        String line = "" + S.charAt(0) + S.charAt(1);
-        int all     = 1;
-
-        for (int i = 2; i < n; i++) {
-
-            int max   = 0;
-            int index = 0;
-            int m     = line.length();
-
-            for (int j = 1; j < m; j++) {
-                int pts = 0;
-
-                pts += line.indexOf('R', j) == -1 ? 0 : 1;
-                pts += line.indexOf('G', j) == -1 ? 0 : 1;
-                pts += line.indexOf('B', j) == -1 ? 0 : 1;
-
-                pts += line.substring(0, j).indexOf('R') == -1 ? 0 : 1;
-                pts += line.substring(0, j).indexOf('G') == -1 ? 0 : 1;
-                pts += line.substring(0, j).indexOf('B') == -1 ? 0 : 1;
-
-                if (pts > max) {
-                    max = pts;
-                    index = j;
-                }
-            }
-
-            line = line.substring(0, index) + S.charAt(i) + line.substring(index);
-            all += max;
+        int count = 0;
+        for (char c : chars) {
+            count += left.size();
+            count += right.size();
+            (left.contains(c) ? right : left).add(c);
         }
 
-        return all;
+        return count;
     }
 
     private void debug(Object... os) {
