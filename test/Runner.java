2becdda74e971dc6e9715e5245db18de46ec62b2
Gabriel Adomnicai <gabesoft@gmail.com>
2014-07-22 16:22:51 -0700

    manysquares - complete

--- test/Runner.java
+++ test/Runner.java
@@ -4,9 +4,9 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import topc.test.easy.*;
+import topc.test.geometry.*;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ WinterAndPresentsTest.class })
+@Suite.SuiteClasses({ ManySquaresTest.class })
 public class Runner {
 }
