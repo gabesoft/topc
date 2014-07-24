f4556961e281c803b99adbc16194f9e008aa7ac6
Gabriel Adomnicai <gabesoft@gmail.com>
2014-07-24 08:54:01 -0700

    switchinggame - complete

--- test/Runner.java
+++ test/Runner.java
@@ -4,9 +4,9 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import topc.test.search.*;
+import topc.test.greedy.*;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ BuildingHeightsTest.class })
+@Suite.SuiteClasses({ SwitchingGameTest.class })
 public class Runner {
 }
