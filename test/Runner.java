7abf596195c0f647ef28c05b28fb5ec533f903fe
Gabriel Adomnicai <gabesoft@gmail.com>
2014-07-27 16:30:27 -0700

    subsets - complete

--- test/Runner.java
+++ test/Runner.java
@@ -4,9 +4,9 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import topc.test.greedy.*;
+import topc.test.math.*;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ BoxesDiv2Test.class })
+@Suite.SuiteClasses({ SubsetsTest.class })
 public class Runner {
 }
