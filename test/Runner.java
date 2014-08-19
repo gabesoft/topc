72c572a76ca63b7462241ec4e659357ed6484a0e
Gabriel Adomnicai <gabesoft@gmail.com>
2014-08-19 11:30:46 -0700

    leftandrighthandeddiv2 - complete

--- test/Runner.java
+++ test/Runner.java
@@ -4,9 +4,9 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import topc.test.dynamic.*;
+import topc.test.greedy.*;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ TaroCardsTest.class })
+@Suite.SuiteClasses({ LeftAndRightHandedDiv2Test.class })
 public class Runner {
 }
