482156d2ef71d6ad4e243912a96a0287c1096202
Gabriel Adomnicai <gabesoft@gmail.com>
2014-08-19 16:18:36 -0700

    interestingnumber - complete

--- test/Runner.java
+++ test/Runner.java
@@ -4,9 +4,9 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import topc.test.dynamic.*;
+import topc.test.easy.*;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ EmoticonsDiv1Test.class })
+@Suite.SuiteClasses({ InterestingNumberTest.class })
 public class Runner {
 }
