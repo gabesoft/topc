86261305612f6266ce9f5f4294fcff1f25c729c3
Gabriel Adomnicai <gabesoft@gmail.com>
2014-08-25 12:41:04 -0700

    palindromicsubstringsdiv2 - complete

--- src/dynamic/PalindromicSubstringsDiv2.java
+++ src/dynamic/PalindromicSubstringsDiv2.java
@@ -8,50 +8,49 @@ import java.io.*;
 // statement: http://community.topcoder.com/stat?c=problem_statement&pm=12967&rd=15840
 // editorial: http://apps.topcoder.com/wiki/display/tc/SRM+607
 public class PalindromicSubstringsDiv2 {
-    public int count(String[] S1, String[] S2) {
-        String s = join(S1) + join(S2);
-        int n = s.length();
-        int c = 0;
+  int dp[][];
 
-        for (int i = 1; i <= n; i++) {
-            c += pali(s, i);
+  public int count(String[] S1, String[] S2) {
+    String s = join(S1) + join(S2);
+    char[] chars = s.toCharArray();
+    int n = s.length();
+    int c = 0;
+
+    dp = new int[n + 1][n + 1];
+
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j <= n; j++) {
+        if (pali(chars, i, j)) {
+          c++;
         }
-
-        return c;
+      }
     }
 
-    private int pali(String s, int l) {
-        int n = s.length();
-        int c = 0;
-        for (int i = 0; i <= (n - l); i++) {
-            if (pali(s.substring(i, i + l))) {
-                c++;
-            }
-        }
-        return c;
+    return c;
+  }
+
+  private boolean pali(char[] s, int a, int b) {
+    if (dp[a][b] > 0) { return dp[a][b] == 1; }
+    if (b - a <= 1) { return true; }
+
+    boolean res = false;
+    if (s[a] == s[b - 1]) {
+      res = pali(s, a + 1, b - 1);
     }
 
-    private boolean pali(String s) {
-        int n = s.length();
+    dp[a][b] = res ? 1 : 2;
+    return res;
+  }
 
-        for (int i = 0; i < n / 2; i++) {
-            if (s.charAt(i) != s.charAt(n - i - 1)) {
-                return false;
-            }
-        }
-
-        return true;
+  String join(String[] input) {
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < input.length; i++) {
+      builder.append(input[i]);
     }
+    return builder.toString();
+  }
 
-    String join(String[] input) {
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < input.length; i++) {
-            builder.append(input[i]);
-        }
-        return builder.toString();
-    }
-
-    private void debug(Object... os) {
-        System.out.println(Arrays.deepToString(os));
-    }
+  private void debug(Object... os) {
+    System.out.println(Arrays.deepToString(os));
+  }
 }
