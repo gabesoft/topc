3950a8e7dad4eb3b6be224b52db4ba056448baa6
Gabriel Adomnicai <gabesoft@gmail.com>
2014-09-19 18:08:38 -0700

    excavations2 - not started

---
+++ src/dynamic/Excavations2.java
@@ -0,0 +1,18 @@
+package topc.dynamic;
+
+import java.util.*;
+import java.io.*;
+
+// SRM 584 Division II Level Three - 1000
+// dynamic programming
+// statement: http://community.topcoder.com/stat?c=problem_statement&pm=12644&rd=15696
+// editorial: http://apps.topcoder.com/wiki/display/tc/SRM+584
+public class Excavations2 {
+  public long count(int[] kind, int[] found, int K) {
+    return 0L;
+  }
+
+  private void debug(Object... os) {
+    System.out.println(Arrays.deepToString(os));
+  }
+}
