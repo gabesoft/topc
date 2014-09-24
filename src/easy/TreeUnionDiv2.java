ccbe9b2874dc130e97a62a0a05a81695634c2178
Gabriel Adomnicai <gabesoft@gmail.com>
2014-09-23 17:42:11 -0700

    treeuniondiv2 - not started

---
+++ src/easy/TreeUnionDiv2.java
@@ -0,0 +1,18 @@
+package topc.easy;
+
+import java.util.*;
+import java.io.*;
+
+// SRM 581 Division II Level Three - 1000
+// brute force, graph theory
+// statement: http://community.topcoder.com/stat?c=problem_statement&pm=12587&rd=15501
+// editorial: http://apps.topcoder.com/wiki/display/tc/SRM+581
+public class TreeUnionDiv2 {
+  public int maximumCycles(String[] tree1, String[] tree2, int K) {
+    return 0;
+  }
+
+  private void debug(Object... os) {
+    System.out.println(Arrays.deepToString(os));
+  }
+}
