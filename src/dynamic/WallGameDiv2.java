5c314d8386c2931d3a0bdb45dd49b8922f7c338c
Gabriel Adomnicai <gabesoft@gmail.com>
2014-09-24 21:43:25 -0700

    wallgamediv2 - in progress

---
+++ src/dynamic/WallGameDiv2.java
@@ -0,0 +1,22 @@
+package topc.dynamic;
+
+import java.util.*;
+import java.io.*;
+
+// SRM 580 Division II Level Three - 1000
+// dynamic programming
+// statement: http://community.topcoder.com/stat?c=problem_statement&pm=12579&rd=15500
+// editorial: http://apps.topcoder.com/wiki/display/tc/SRM+580
+public class WallGameDiv2 {
+    public int play(String[] costs) {
+        return 0;
+    }
+
+    private int max(int r, int c) {
+        if (r == n - 1 && c == m - 1) { return 0; }
+    }
+
+    private void debug(Object... os) {
+        System.out.println(Arrays.deepToString(os));
+    }
+}
