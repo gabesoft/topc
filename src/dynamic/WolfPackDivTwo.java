18d3bc004b1683d55ccaa671c54c781f01850771
Gabriel Adomnicai <gabesoft@gmail.com>
2014-09-29 21:11:11 -0700

    wolfpackdivtwo - not started

---
+++ src/dynamic/WolfPackDivTwo.java
@@ -0,0 +1,18 @@
+package topc.dynamic;
+
+import java.util.*;
+import java.io.*;
+
+// SRM 573 Division II Level Three - 1000
+// dynamic programming, math
+// statement: http://community.topcoder.com/stat?c=problem_statement&pm=12467&rd=15493
+// editorial: http://apps.topcoder.com/wiki/display/tc/SRM+573
+public class WolfPackDivTwo {
+    public int calc(int[] x, int[] y, int m) {
+        return 0;
+    }
+
+    private void debug(Object... os) {
+        System.out.println(Arrays.deepToString(os));
+    }
+}
