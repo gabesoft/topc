cf13f33b4f0b4ade6f386a1bfd4da25f5d81dac7
Gabriel Adomnicai <gabesoft@gmail.com>
2014-10-09 13:55:43 -0700

    spellcardseasy - complete

--- src/dynamic/SpellCardsEasy.java
+++ src/dynamic/SpellCardsEasy.java
@@ -8,11 +8,45 @@ import java.io.*;
 // statement: http://community.topcoder.com/stat?c=problem_statement&pm=12333&rd=15185
 // editorial: http://apps.topcoder.com/wiki/display/tc/SRM+563
 public class SpellCardsEasy {
-    public int maxDamage(int[] level, int[] damage) {
-        return 0;
+  int level[] = null;
+  int damage[] = null;
+  int n = 0;
+  int mem[][] = null;
+
+  public int maxDamage(int[] level, int[] damage) {
+    this.level = level;
+    this.damage = damage;
+    this.n = level.length;
+    this.mem = new int[n][n];
+
+    for (int[] m : mem) {
+      Arrays.fill(m, -1);
     }
 
-    private void debug(Object... os) {
-        System.out.println(Arrays.deepToString(os));
+    return max(0, 0);
+  }
+
+  private int max(int index, int left) {
+    if (left < 0 || left > n - index) { return -1; }
+    if (index == n) { return 0; }
+    if (mem[index][left] > -1) { return mem[index][left]; }
+
+    int best = max(index + 1, Math.max(0, left - 1));
+    int available = n - index - 1;
+    int required = level[index] - 1;
+
+    for (int i = available - left; i >= required; i--) {
+      int next = max(index + 1, left + i);
+      if (next > -1) {
+        best = Math.max(best, damage[index] + next);
+      }
     }
+
+    mem[index][left] = best;
+    return best;
+  }
+
+  private void debug(Object... os) {
+    System.out.println(Arrays.deepToString(os));
+  }
 }
