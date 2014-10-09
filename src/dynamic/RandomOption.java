d4ffaec92dbe4cea1980e8476fbc6794f29251f8
Gabriel Adomnicai <gabesoft@gmail.com>
2014-10-09 16:12:38 -0700

    randomoption - not started

---
+++ src/dynamic/RandomOption.java
@@ -0,0 +1,18 @@
+package topc.dynamic;
+
+import java.util.*;
+import java.io.*;
+
+// SRM 562 Division II Level Three - 900
+// dynamic programming, graph theory
+// statement: http://community.topcoder.com/stat?c=problem_statement&pm=12315&rd=15184
+// editorial: http://apps.topcoder.com/wiki/display/tc/SRM+562
+public class RandomOption {
+  public double getProbability(int keyCount, int[] badLane1, int[] badLane2) {
+    return 0.0;
+  }
+
+  private void debug(Object... os) {
+    System.out.println(Arrays.deepToString(os));
+  }
+}
