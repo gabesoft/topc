2a49865b88195c181044cf3d2051bb5e78fd6622
Gabriel Adomnicai <gabesoft@gmail.com>
2014-10-09 08:35:09 -0700

    spellcardseasy - not started

---
+++ src/dynamic/SpellCardsEasy.java
@@ -0,0 +1,18 @@
+package topc.dynamic;
+
+import java.util.*;
+import java.io.*;
+
+// SRM 563 Division II Level Three - 1000
+// dynamic programming
+// statement: http://community.topcoder.com/stat?c=problem_statement&pm=12333&rd=15185
+// editorial: http://apps.topcoder.com/wiki/display/tc/SRM+563
+public class SpellCardsEasy {
+    public int maxDamage(int[] level, int[] damage) {
+        return 0;
+    }
+
+    private void debug(Object... os) {
+        System.out.println(Arrays.deepToString(os));
+    }
+}
--- test/Runner.java
+++ test/Runner.java
@@ -4,9 +4,9 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import topc.test.easy.*;
+import topc.test.dynamic.*;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ CoinsGameEasyTest.class })
+@Suite.SuiteClasses({ SpellCardsEasyTest.class })
 public class Runner {
 }
