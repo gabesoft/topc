f0226de93b96293dedf1fce8054aa3c0fbdb8417
Gabriel Adomnicai <gabesoft@gmail.com>
2014-11-19 20:28:36 -0800

    favouritedigits - complete

--- test/Runner.java
+++ test/Runner.java
@@ -4,9 +4,9 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import topc.test.geometry.*;
+import topc.test.greedy.*;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ TwoRectanglesTest.class })
+@Suite.SuiteClasses({ FavouriteDigitsTest.class })
 public class Runner {
 }
