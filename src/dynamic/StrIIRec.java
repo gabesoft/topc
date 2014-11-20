8db1b99cd1c2cf7a8489a6986b82c5f448e65403
Gabriel Adomnicai <gabesoft@gmail.com>
2014-11-20 08:40:12 -0800

    striirec - not started

---
+++ src/dynamic/StrIIRec.java
@@ -0,0 +1,18 @@
+package topc.dynamic;
+
+import java.util.*;
+import java.io.*;
+
+// SRM 545 Division I Level One - 275
+// dynamic programming, greedy, search
+// statement: http://community.topcoder.com/stat?c=problem_statement&pm=12025&rd=14737
+// editorial: http://apps.topcoder.com/wiki/display/tc/SRM+545
+public class StrIIRec {
+    public String recovstr(int n, int minInv, String minStr) {
+        return null;
+    }
+
+    private void debug(Object... os) {
+        System.out.println(Arrays.deepToString(os));
+    }
+}
