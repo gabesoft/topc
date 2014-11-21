af25bbc0848e494eb96be42bfaa8398de9de497d
Gabriel Adomnicai <gabesoft@gmail.com>
2014-11-20 18:07:33 -0800

    alicebobshuffle - not started

---
+++ src/dynamic/AliceBobShuffle.java
@@ -0,0 +1,18 @@
+package topc.dynamic;
+
+import java.util.*;
+import java.io.*;
+
+// SRM 544 Division II Level Three - 1000
+// dynamic programming, simulation
+// statement: http://community.topcoder.com/stat?c=problem_statement&pm=11865&rd=14736
+// editorial: http://apps.topcoder.com/wiki/display/tc/SRM+544
+public class AliceBobShuffle {
+  public int countWays(int[] AliceStart, int[] BobStart, int[] AliceEnd, int[] BobEnd) {
+    return 0;
+  }
+
+  private void debug(Object... os) {
+    System.out.println(Arrays.deepToString(os));
+  }
+}
--- test/Runner.java
+++ test/Runner.java
@@ -4,9 +4,9 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import topc.test.greedy.*;
+import topc.test.dynamic.*;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ BoardSplittingTest.class })
+@Suite.SuiteClasses({ AliceBobShuffleTest.class })
 public class Runner {
 }
