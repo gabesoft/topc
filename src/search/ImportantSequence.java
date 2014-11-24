6ade5b86d964a40251c93a669397b5f638904210
Gabriel Adomnicai <gabesoft@gmail.com>
2014-11-23 17:52:58 -0800

    importantsequence - complete

--- src/search/ImportantSequence.java
+++ src/search/ImportantSequence.java
@@ -8,37 +8,31 @@ import java.io.*;
 // statement: http://community.topcoder.com/stat?c=problem_statement&pm=11842&rd=14732
 // editorial: http://apps.topcoder.com/wiki/display/tc/SRM+540
 public class ImportantSequence {
-    public static final long INF = 1 << 60;
+    public static final long INF = Long.MAX_VALUE;
     public int getCount(int[] B, String operators) {
         int n      = B.length;
-        long[] val  = new long[n];
-        long[] sign = new long[n];
+        long[] val = new long[n];
+        long[] sig = new long[n];
 
-        long op  = operators.charAt(0) == '+' ? 1 : -1;
+        long op = operators.charAt(0) == '+' ? 1 : -1;
         val[0]  = op * B[0];
-        sign[0] = op * -1;
+        sig[0]  = op * -1;
 
         for (int i = 1; i < n; i++) {
-            op = operators.charAt(i) == '+' ? 1 : -1;
-            val[i] = -1 * op * val[i - 1] + op * B[i];
-            sign[i] = sign[i - 1] * -1 * op;
+            op     = operators.charAt(i) == '+' ? 1 : -1;
+            val[i] = op * B[i] + -1 * op * val[i - 1];
+            sig[i] = -1 * op * sig[i - 1];
         }
-
-        debug(B);
-        debug('V', val);
-        debug('S', sign);
 
         long lo = 1;
         long hi = INF;
         for (int i = 0; i < n; i++) {
-            if (sign[i] == -1) {
+            if (sig[i] == -1) {
                 hi = Math.min(hi, val[i] - 1);
-            } else if (sign[i] == 1) {
+            } else if (sig[i] == 1) {
                 lo = Math.max(lo, -1 * val[i] + 1);
             }
         }
-
-        debug(lo, hi, INF);
 
         if (lo > hi) {
             return 0;
