657fcded2b1c4f26a1f68467bba621a733780170
Gabriel Adomnicai <gabesoft@gmail.com>
2014-11-24 12:14:41 -0800

    platypuspaternity - complete

--- test/Runner.java
+++ test/Runner.java
@@ -4,9 +4,9 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import topc.test.math.*;
+import topc.test.search.*;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ FractionInDifferentBasesTest.class })
+@Suite.SuiteClasses({ PlatypusPaternityTest.class })
 public class Runner {
 }
