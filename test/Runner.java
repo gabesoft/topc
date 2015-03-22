76ed1417a6d2c19b1a99fd65d2a869f394fddab9
Gabriel Adomnicai <gabesoft@gmail.com>
2015-03-21 18:04:46 -0700

    thegriddivtwo - complete

--- test/Runner.java
+++ test/Runner.java
@@ -4,9 +4,9 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import topc.test.search.*;
+import topc.test.geometry.*;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ TheConsecutiveIntegersDivTwoTest.class })
+@Suite.SuiteClasses({ TheGridDivTwoTest.class })
 public class Runner {
 }
