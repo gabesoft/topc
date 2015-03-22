bb2d6c1a0828cbe3f72b4cd3272e6ade0b77d3ac
Gabriel Adomnicai <gabesoft@gmail.com>
2015-03-22 00:29:50 -0700

    connectingcars - complete

--- test/Runner.java
+++ test/Runner.java
@@ -4,9 +4,9 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import topc.test.simulation.*;
+import topc.test.search.*;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ BacteriesColonyTest.class })
+@Suite.SuiteClasses({ ConnectingCarsTest.class })
 public class Runner {
 }
