a9c61c20faf3508ba6b823d6a67ec3beeb971631
Gabriel Adomnicai <gabesoft@gmail.com>
2015-05-02 19:56:10 -0700

    norightturndiv2 - complete

--- test/Runner.java
+++ test/Runner.java
@@ -4,9 +4,9 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import topc.test.math.*;
+import topc.test.easy.*;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ ThePermutationGameDiv2Test.class })
+@Suite.SuiteClasses({ NoRightTurnDiv2Test.class })
 public class Runner {
 }
