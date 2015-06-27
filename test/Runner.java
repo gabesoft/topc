68cb9cb3fc7557ee41a07e26bf80b0f41ac65608
Gabriel Adomnicai <gabesoft@gmail.com>
2015-06-27 15:51:12 -0700

    Coversta - complete

--- test/Runner.java
+++ test/Runner.java
@@ -7,6 +7,6 @@ import org.junit.runners.Suite;
 import topc.test.easy.*;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ PoweritTest.class })
+@Suite.SuiteClasses({ CoverstaTest.class })
 public class Runner {
 }
