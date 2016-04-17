59d4926d7b1cd192cef70f6595f7ac54c4d5ba7a
Gabriel Adomnicai <gabesoft@gmail.com>
2016-04-17 12:22:12 -0700

    BreedingLikeRabbits - complete

--- src/topc/easy/BreedingLikeRabbits.java
+++ src/topc/easy/BreedingLikeRabbits.java
@@ -6,10 +6,10 @@ import java.math.BigInteger;
 
 public class BreedingLikeRabbits {
     public static String answer(String str_S) {
-        java.util.HashMap<java.math.BigInteger, java.math.BigInteger> map = new java.util.HashMap<java.math.BigInteger, java.math.BigInteger>();
+        HashMap<BigInteger, BigInteger> map = new HashMap<BigInteger, BigInteger>();
 
-        java.math.BigInteger val = new java.math.BigInteger(str_S);
-        java.math.BigInteger res = search(map, val, false);
+        BigInteger val = new BigInteger(str_S);
+        BigInteger res = search(map, val, false);
 
         if (res == null) {
             res = search(map, val, true);
@@ -18,11 +18,11 @@ public class BreedingLikeRabbits {
         return (res == null) ? "None" : res.toString(10);
     }
 
-    private static java.math.BigInteger search(java.util.HashMap<java.math.BigInteger, java.math.BigInteger> map, java.math.BigInteger target, boolean even) {
-        java.math.BigInteger two = java.math.BigInteger.valueOf(2);
-        java.math.BigInteger one = java.math.BigInteger.ONE;
-        java.math.BigInteger lo = even ? java.math.BigInteger.ZERO : java.math.BigInteger.ONE;
-        java.math.BigInteger hi = target.multiply(java.math.BigInteger.TEN);
+    private static BigInteger search(HashMap<BigInteger, BigInteger> map, BigInteger target, boolean even) {
+        BigInteger two = BigInteger.valueOf(2);
+        BigInteger one = BigInteger.ONE;
+        BigInteger lo = even ? BigInteger.ZERO : BigInteger.ONE;
+        BigInteger hi = target.multiply(BigInteger.TEN);
 
         if ((even && !isEven(hi)) || (!even && isEven(hi))) {
             hi = hi.add(one);
@@ -31,13 +31,13 @@ public class BreedingLikeRabbits {
         int count = 0;
 
         while (lo.compareTo(hi) < 0 && count++ < 100) {
-            java.math.BigInteger mid = lo.add(hi.subtract(lo).divide(two));
+            BigInteger mid = lo.add(hi.subtract(lo).divide(two));
 
             if ((even && !isEven(mid)) || (!even && isEven(mid))) {
                 mid = mid.add(one);
             }
 
-            java.math.BigInteger next = R(map, mid);
+            BigInteger next = R(map, mid);
 
             if (next.compareTo(target) == 0) {
                 return mid;
@@ -51,35 +51,35 @@ public class BreedingLikeRabbits {
         return null;
     }
 
-    private static java.math.BigInteger R(java.util.HashMap<java.math.BigInteger, java.math.BigInteger> map,  java.math.BigInteger n) {
-        final java.math.BigInteger two = java.math.BigInteger.valueOf(2);
+    private static BigInteger R(HashMap<BigInteger, BigInteger> map,  BigInteger n) {
+        final BigInteger two = BigInteger.valueOf(2);
         if (n.compareTo(two) < 0) {
-            return java.math.BigInteger.ONE;
+            return BigInteger.ONE;
         } else if (n.compareTo(two) == 0) {
-            return java.math.BigInteger.valueOf(2);
+            return BigInteger.valueOf(2);
         } else if (map.containsKey(n)) {
             return map.get(n);
         }
 
-        java.math.BigInteger k = n.divide(two);
-        java.math.BigInteger result = java.math.BigInteger.ZERO;
+        BigInteger k = n.divide(two);
+        BigInteger result = BigInteger.ZERO;
         boolean even = isEven(n);
 
         if (even) {
             result = result.add(R(map, k));
-            result = result.add(R(map, k.add(java.math.BigInteger.ONE)));
+            result = result.add(R(map, k.add(BigInteger.ONE)));
             result = result.add(k);
         } else {
-            result = result.add(R(map, k.subtract(java.math.BigInteger.ONE)));
+            result = result.add(R(map, k.subtract(BigInteger.ONE)));
             result = result.add(R(map, k));
-            result = result.add(java.math.BigInteger.ONE);
+            result = result.add(BigInteger.ONE);
         }
 
         map.put(n, result);
         return result;
     }
 
-    private static boolean isEven(java.math.BigInteger n) {
+    private static boolean isEven(BigInteger n) {
         return n.getLowestSetBit() != 0;
     }
 
